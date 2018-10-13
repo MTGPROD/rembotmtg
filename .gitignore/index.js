290742d285ce741db0ad506b566020006840917e
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-13 16:42:38 +0200

    Commands handler

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,9 +2,27 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const prefix = "r!";
-const ownerID = '';
+const ownerID = '234234723314958339';
 
 client.on('message', message => {
+    let args = message.content.slice(prefix.length).trim().split(' ');
+    let cmd = args.shift().toLowerCase();
+
+    if(message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+
+    try {
+        delete require.cache[require.resolve(`./commands/${cmd}.js`)]
+
+        let ops = {
+            ownerID: ownerID
+        }
+
+        let commandFile = require(`./commands/${cmd}.js`)
+        commandFile.run(client, message, args, ops);
+    } catch (e) {
+        console.log(e.stack)
+    }
 });
 
 client.on('ready', () => console.log('My body is ready'));
