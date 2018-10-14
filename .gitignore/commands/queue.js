85496e55e312c43914855a47fba2868a321781c6
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-14 21:07:13 +0200

    Create queue.js

---
+++ .gitignore/commands/queue.js
@@ -0,0 +1,11 @@
+exports.run = async (client, message, args, ops) => {
+  let fetched = ops.active.get(message.guild.id);
+  if(!fetched) return message.channel.send('There currently isn\'t any music playing in this guild !');
+  let queue = fetched.queue;
+  let nowPlaying = queue[0];
+  let resp = `__**Now Playing**__\n**${nowPlaying.songTitle}** -- **Requested By:** *${nowPlaying.requester}*\n\n__**Queue**__\n`;
+  for(var i = 1; i < queue.length; i++) {
+    resp += `${i}. **${queue[i].songTitle}** -- **Requested By:** *${queue[i].requester}*\n`;
+  }
+  message.channel.send(resp);
+}
