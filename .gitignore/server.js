b4949bdeaa4245204a32a12601c8f26b11129667
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-17 00:41:11 +0200

    Update server.js

--- .gitignore/server.js
+++ .gitignore/server.js
@@ -30,104 +30,7 @@ client.on('ready', () => {
 const db = require('quick.db')
 
 
-client.on('message', async message => {
-
-    /*if(message.author.bot) return;
-    if(message.channel.type !== 'text') {
-      let active = await db.fetch(`Support_${message.author.id}`);
-      let guild = client.guilds.get('guildID');
-      let channel, found = true;
-      try {
-        if(active) client.channels.get(active.channelID).guild;
-      } catch (e) {
-        found = false;
-      }
-      if(!active || !found) {
-        active = {};
-        channel = await guild.channels.create(`${message.author.username}-${message.author.discriminator}`, {
-            parent: 'categoryID',
-            topic: `?complete to close the ticket | Support for ${message.author.tag} | ID: ${message.author.id}`
-        });
-        let author = message.author;
-
-        const newChannel = new Discord.RichEmbed()
-            .setColor(color)
-            .setAuthor(author.tag, author.displayAvatarURL())
-            .setFooter('Support Ticket Created')
-            .addField('User', author)
-            .addField('ID', author.id)
-
-        await channel.send(newChannel)
-
-        const newTicket = new Discord.RichEmbed()
-            .setColor(color)
-            .setAuthor(`Hello, ${author.tag}`, author.displayAvatarURL)
-            .setFooter('Support Ticket Created')
-
-        await author.send(newTicket);
-
-        active.channelID = channel.id;
-        active.targetID = author.id;
-      }
-
-      channel = client.channels.get(active.channelID);
-
-      const dw = new Discord.RichEmbed()
-          .setColor(color)
-          .setAuthor(`Thank you, ${message.author.tag}`, message.author.displayAvatarURL())
-          .setFooter(`Your message has been sent -- A staff member will be in contact soon`)
-
-      await message.author.send(dm);
-
-      const embed = new Discord.RichEmbed()
-          .setColor(color)
-          .setAuthor(message.author.tag, message.author.displayAvatarURL())
-          .setDescription(message.content)
-          .setFooter(`Message Recieved -- ${message.author.tag}`)
-
-      await channel.send(embed);
-
-      db.set(`support_${message.author.id}`, active);
-      db.set(`supportChannel_${channel.id}`, message.author.id);
-      return;
-    }
-
-    let support = await db.fetch(`supportChannel_${message.channel.id}`);
-
-    if(support) {
-      support = await db.fetch(`support_${support}`);
-      let supportUser = client.users.get(support.targetID);
-      if(!supportUser) return message.channel.delete();
-      if(message.content.toLowerCase() === '?complete') {
-          const complete = new Discord.RichEmbed()
-              .setColor(color)
-              .setAuthor(`Hey, ${supportUser.tag}`, supportUser.displayAvatarURL())
-              .setFooter('Ticket Closed -- MTG\'s dev')
-              .setDescription('*Your ticket has been marked as **complete**. If you wish to reopen this, or create a new one, please send a message to the bot.')
-
-          supportUser.send(complete);
-
-          message.channel.delete();
-
-          db.delete(`Support_${support.targetID}`);
-      }
-
-      const embed2 = new Discord.RichEmbed()
-          .setColor(color)
-          .setAuthor(message.author.tag, message.author.displayAvatarURL())
-          .setFooter(`Message Recived -- MTG's dev`)
-          .setDescription(message.content)
-
-      client.users.get(support.targetID).send(embed)
-
-      message.delete({timeout: 1000});
-
-      embed2.setFooter(`Message Sent -- ${supportUser.tag}`).setDescription(message.content);
-
-      return message.channel.send(embed);
-    }*/
-
-
+client.on('message', message => {
 
     let args = message.content.slice(prefix.length).trim().split(' ');
     let cmd = args.shift().toLowerCase();
@@ -182,3 +85,4 @@ client.on('guildDelete', guild => {
 
 
 client.login(process.env.TOKEN)
+})
