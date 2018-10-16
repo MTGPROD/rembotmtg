f376691b4fe205a56127e71a5aa54163e3419640
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-17 01:56:05 +0200

    Update help.js

--- .gitignore/commands/help.js
+++ .gitignore/commands/help.js
@@ -8,7 +8,7 @@ exports.run = (client, message, args, ops) => {
         .addField('Basics', `${prefix}ascii <text>\n${prefix}avatar\n${prefix}calc\n${prefix}cat\n${prefix}color\n${prefix}colors\n${prefix}findusers\n${prefix}help\n${prefix}ping\n${prefix}poll\n${prefix}purge\n${prefix}spotify`)
         .addBlankField(true)
         .addField('Music', `${prefix}play\n${prefix}search\n${prefix}leave\n${prefix}pause\n${prefix}resume\n${prefix}volume\n${prefix}queue`)
-        .setFooter(message.author.username + message.author.displayavatarURL())
+        .setFooter(`Rem 2.0`)
 
     message.channel.send({embed: embed});
 }
