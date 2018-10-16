87afaf196ecbd087b7741f42fd6922f33fe7d41f
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-17 01:53:40 +0200

    Create help.js

---
+++ .gitignore/commands/help.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const prefix = 'r!'
+
+exports.run = (client, message, args, ops) => {
+    const embed = new Discord.RichEmbed()
+        .setTitle(`Help requested by ${message.author.username}`)
+        .setThumbnail(message.author.avatarURL)
+        .addField('Basics', `${prefix}ascii <text>\n${prefix}avatar\n${prefix}calc\n${prefix}cat\n${prefix}color\n${prefix}colors\n${prefix}findusers\n${prefix}help\n${prefix}ping\n${prefix}poll\n${prefix}purge\n${prefix}spotify`)
+        .addBlankField(true)
+        .addField('Music', `${prefix}play\n${prefix}search\n${prefix}leave\n${prefix}pause\n${prefix}resume\n${prefix}volume\n${prefix}queue`)
+        .setFooter(message.author.username + message.author.displayavatarURL())
+
+    message.channel.send({embed: embed});
+}
