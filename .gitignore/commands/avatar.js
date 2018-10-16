c81fdfba9444ece9ec9e811c7045d072411bb49a
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-17 00:48:31 +0200

    Create avatar.js

---
+++ .gitignore/commands/avatar.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, message, args, tools) => {
+    let msg = await message.channel.send("Loading...")
+    let target = message.mentions.users.first() || message.author;
+ 
+    const embed = new Discord.RichEmbed()
+        .setTitle(`**${target.username}'s** avatar !`)
+        .setURL('https://google.com')
+        .setColor(color)
+        .setImage(target.avatarURL)
+        .setFooter(`Requested by ${message.author.username}`)
+
+    await message.channel.send({embed: embed});
+    
+    msg.delete();
+}
