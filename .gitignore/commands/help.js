36f7505e61d67f5c67b4cbb03205abbbc7c2e45c
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-17 02:00:25 +0200

    Update help.js

--- .gitignore/commands/help.js
+++ .gitignore/commands/help.js
@@ -3,11 +3,14 @@ const prefix = 'r!'
 
 exports.run = (client, message, args, ops) => {
     const embed = new Discord.RichEmbed()
-        .setTitle(`Help requested by ${message.author.username}`)
+        .setTitle(`Help requested by **${message.author.username}**`)
+        .setURL('https://google.com/')
+        .setColor('#329FFE')
         .setThumbnail(message.author.avatarURL)
-        .addField('Basics', `${prefix}ascii <text>\n${prefix}avatar\n${prefix}calc\n${prefix}cat\n${prefix}color\n${prefix}colors\n${prefix}findusers\n${prefix}help\n${prefix}ping\n${prefix}poll\n${prefix}purge\n${prefix}spotify`)
+        .addField('Basics', `${prefix}ascii <text>\n${prefix}avatar @user\n${prefix}calc <number +-*%/ number>\n${prefix}cat\n${prefix}color\n${prefix}colors\n${prefix}findusers\n${prefix}help\n${prefix}ping\n${prefix}poll\n${prefix}purge\n${prefix}spotify`)
         .addBlankField(true)
-        .addField('Music', `${prefix}play\n${prefix}search\n${prefix}leave\n${prefix}pause\n${prefix}resume\n${prefix}volume\n${prefix}queue`)
+        .addField('Music', `${prefix}play\n${prefix}search\n${prefix}leave\n${prefix}pause\n${prefix}resume\n${prefix}volume 0 - 200\n${prefix}queue`)
+        .setTimestamp()
         .setFooter(`Rem 2.0`)
 
     message.channel.send({embed: embed});
