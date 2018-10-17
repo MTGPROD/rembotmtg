86678c6bcc73e351289e75c3f1921c52b7a31fd7
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-17 02:10:28 +0200

    Update volume.js

--- .gitignore/commands/volume.js
+++ .gitignore/commands/volume.js
@@ -1,6 +1,8 @@
 exports.run = (client, message, args, ops) => {
-    let fetched = ops.active.get(message.guild.id);
-    if(!fetched) return message.channel.send('There currently isn\'t any music playing in this guild !');
-    if(message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send("Sorry, you aren't in the same channel as the music bot.");
-    if(fetched.dispatcher.paused) return fetched.dispatcher.resume(); 
-  }
+  let fetched = ops.active.get(message.guild.id);
+  if(!fetched) return message.channel.send("There currently ins't any music playing in this guild !");
+  if(message.member.voiceChannel !== message.guild.me.voiceChannel) return message.chanel.send("Sorry, you currently aren't in the same channel as the bot.")
+  if(isNaN(args[0]) || args[0] > 200 || args[0] < 0) return message.channel.send('Please input a number between `0-200`');
+  fetched.dispatcher.setVolume(args[0]/100);
+  message.channel.send(`Successfully set the volume of ${fetched.queue[0].songTitle} to \`${args[0]}\``)
+}
