c81a7ea0c2c14d48b3772fa151a1349f8100b40e
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-17 15:22:56 +0200

    Update help.js

--- .gitignore/commands/help.js
+++ .gitignore/commands/help.js
@@ -7,7 +7,7 @@ exports.run = (client, message, args, ops) => {
         .setURL('https://google.com/')
         .setColor('#329FFE')
         .setThumbnail(message.author.avatarURL)
-        .addField('Basics', `${prefix}ascii <text>\n${prefix}avatar @user\n${prefix}calc <number +-*%/ number>\n${prefix}cat\n${prefix}color\n${prefix}colors\n${prefix}findusers\n${prefix}help\n${prefix}ping\n${prefix}poll\n${prefix}purge\n${prefix}spotify\n${prefix}csgo <user>`)
+        .addField('Basics', `${prefix}ascii <text>\n${prefix}avatar @user\n${prefix}calc <number +-*%/ number>\n${prefix}cat\n${prefix}color\n${prefix}colors\n${prefix}findusers\n${prefix}help\n${prefix}ping\n${prefix}poll\n${prefix}purge\n${prefix}spotify\n${prefix}csgo <user>\n${prefix}fornite\n`)
         .addBlankField(true)
         .addField('Music', `${prefix}play\n${prefix}search\n${prefix}leave\n${prefix}pause\n${prefix}resume\n${prefix}volume 0 - 200\n${prefix}queue`)
         .setTimestamp()
