1a72cee41cb06b762178226cf7640378095b0e8c
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-17 14:01:57 +0200

    Update ping.js

--- .gitignore/commands/ping.js
+++ .gitignore/commands/ping.js
@@ -1,8 +1,24 @@
-const Discord = require('discord.js')
+const Discord = require("discord.js");
 
-exports.run = (client, message, args) => {
-    const embed = new Discord.RichEmbed()
-        .setDescription(':ping_pong: pong')
+exports.run = async (bot, message, args) => {
+    
+    
 
-    message.channel.send({embed: embed})
-}
+    let msgping1 = new Date();
+
+    let botping = new Date() - message.createdAt;
+
+    let msgping2 = new Date() - msgping1;
+
+    let pingembed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .addField(':heartbeat: API Ping : ', Math.floor(bot.ping) + 'ms')
+        .addField(':robot: Bot Ping : ', Math.floor(botping) + 'ms')
+        .addField(':envelope: Message Ping : ', '~' + Math.round(msgping2) + 'ms')
+        .setTimestamp(new Date())
+        .setFooter(`requested by ${message.author.tag}`);
+
+        
+    return message.channel.send(pingembed);
+       
+};
