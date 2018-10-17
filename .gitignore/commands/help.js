2f55048d93cd67f341adabc424989a5c736df708
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-17 20:34:08 +0200

    Update help.js

--- .gitignore/commands/help.js
+++ .gitignore/commands/help.js
@@ -1,15 +1,23 @@
 const Discord = require('discord.js');
 const prefix = 'r!'
 
+let basics = ['ping', 'botstats', 'post', 'purge']
+let fun = ['8ball', 'ascii', 'b64encode', 'b64decode', 'calc', 'fortnite', 'csgo', 'animes', 'cat']
+let specials = ['colors', 'color', 'poll']
+let music = ['play', 'search', 'leave', 'pause', 'resume', 'volume', 'queue']
+let modération = ['ban', 'unmute', 'mute', 'kick']
+
 exports.run = (client, message, args, ops) => {
     const embed = new Discord.RichEmbed()
         .setTitle(`Help requested by **${message.author.username}**`)
         .setURL('https://google.com/')
         .setColor('#329FFE')
         .setThumbnail(message.author.avatarURL)
-        .addField('Basics', `${prefix}ascii <text>\n${prefix}avatar @user\n${prefix}calc <number +-*%/ number>\n${prefix}cat\n${prefix}color\n${prefix}colors\n${prefix}findusers\n${prefix}help\n${prefix}ping\n${prefix}poll\n${prefix}purge\n${prefix}spotify\n${prefix}csgo <user>\n${prefix}fornite\n`)
-        .addBlankField(true)
-        .addField('Music', `${prefix}play\n${prefix}search\n${prefix}leave\n${prefix}pause\n${prefix}resume\n${prefix}volume 0 - 200\n${prefix}queue`)
+        .addField('Basics', `\`${basics}\``)
+        .addField('Fun', `\`${fun}\``)
+        .addField('Specials', `\`${specials}\``)
+        .addField('Music', `\`${music}\``)
+        .addField('Modération', `\`${modération}\``)
         .setTimestamp()
         .setFooter(`Rem 2.0`)
 
