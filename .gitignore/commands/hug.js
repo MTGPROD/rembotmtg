85eb8812ebe473e32d64578300f792a28a3fde03
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-19 21:57:54 +0200

    Create hug.js

---
+++ .gitignore/commands/hug.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+const infos = require('./infos.json');
+
+exports.run = async (client, message, args) => {
+
+    let hugUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!hugUser) return message.channel.send("You are so alone...");
+
+    const {body} = await superagent
+    .get(`https://nekos.life/api/v2/img/hug`);
+
+    let hugEmbed = new Discord.RichEmbed()
+    .setDescription(`\`${message.author.username}\` hugged \`${message.mentions.users.first().username}\` !`)
+    .setImage(body.url)
+    .setColor("RANDOM")
+    .setFooter(infos.version, client.user.displayAvatarURL);
+
+    message.channel.send(hugEmbed)
+
+}
