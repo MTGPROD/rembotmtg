cff7c73a81d80745d715554d5c027b5096c819cd
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-20 13:07:11 +0200

    Update calc.js

--- .gitignore/commands/calc.js
+++ .gitignore/commands/calc.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js');
 const math = require('mathjs');
+const infos = require('./infos.json');
 
 exports.run = async (client, message, args, tools) => {
-    if(!args[0]) return message.channel.send('Please input a calculation.');
+    if(!args[0]) return message.channel.send('Please input a calculation.\n`EX: r!calc 78 * 54 or 4 + 6 etc...`');
     let resp;
     try {
         resp = math.eval(args.join(' '));
@@ -15,6 +16,7 @@ exports.run = async (client, message, args, tools) => {
         .setTitle('Math Calculation')
         .addField('Input', `\`\`\`JS\n${args.join(' ')}\`\`\``)
         .addField('Output', `\`\`\`JS\n${resp}\`\`\``)
+        .setFooter(infos.version, client.user.displayAvatarURL)
 
-    message.channel.send({embed: embed});
+    message.channel.send(embed);
 }
