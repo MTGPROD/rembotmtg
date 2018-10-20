b719348556b3b006fcf91eab113884f7bb8d85b2
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-20 12:53:48 +0200

    Update avatar.js

--- .gitignore/commands/avatar.js
+++ .gitignore/commands/avatar.js
@@ -1,18 +1,17 @@
 const Discord = require('discord.js');
-const color = '#329FFE';
 
 exports.run = async (client, message, args, tools) => {
-    let msg = await message.channel.send("Loading...")
+    let msg = await message.channel.send("<a:Load:491296766835294208> Loading...")
     let target = message.mentions.users.first() || message.author;
  
     const embed = new Discord.RichEmbed()
         .setTitle(`**${target.username}'s** avatar !`)
         .setURL('https://google.com')
-        .setColor(color)
+        .setColor('RANDOM')
         .setImage(target.avatarURL)
-        .setFooter(`Requested by ${message.author.username}`)
+        .setFooter(`Requested by ${message.author.username}`, message.author.displayAvatarURL)
 
-    await message.channel.send({embed: embed});
+    await message.channel.send(embed);
     
     msg.delete();
 }
