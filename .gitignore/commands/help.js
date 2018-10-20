a9af3ec7c8651b6547d074db8aff27ac81c14082
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-20 12:24:52 +0200

    Update help.js

--- .gitignore/commands/help.js
+++ .gitignore/commands/help.js
@@ -1,25 +1,39 @@
 const Discord = require('discord.js');
-const prefix = 'r!'
-
-let basics = ['ping', 'botstats', 'post', 'purge', 'urban', 'animes', 'avatar']
-let fun = ['8ball', 'ascii', 'b64encode', 'b64decode', 'calc', 'fortnite', 'csgo', 'animes', 'cat', 'meme', 'hug', 'kiss']
-let specials = ['colors', 'color', 'poll', 'roleinfo']
-let music = ['play', 'search', 'leave', 'pause', 'resume', 'volume', 'queue']
-let modération = ['ban', 'unmute', 'mute', 'kick', 'unban']
+const infos = require('./infos.json');
+const cmds = require('./commands.json');
 
 exports.run = (client, message, args, ops) => {
-    const embed = new Discord.RichEmbed()
+    if(message.author.id === infos.owners.MTG || infos.owners.Xeno) {
+        const ownerembed = new Discord.RichEmbed()
         .setTitle(`Help requested by **${message.author.username}**`)
         .setURL('https://google.com/')
         .setColor('#329FFE')
         .setThumbnail(message.author.avatarURL)
-        .addField('Basics', `\`${basics}\``)
-        .addField('Fun', `\`${fun}\``)
-        .addField('Specials', `\`${specials}\``)
-        .addField('Music', `\`${music}\``)
-        .addField('Modération', `\`${modération}\``)
+        .addField('Basics', `\`${cmds.Basics}\``)
+        .addField('Fun', `\`${cmds.Fun}\``)
+        .addField('Music', `\`${cmds.Music}\``)
+        .addField('Moderation', `\`${cmds.Moderation}\``)
+        .addField('Others', `\`${cmds.Others}\``)
+        .addField('Dev cmds', `\`${cmds.Dev}\``)
         .setTimestamp()
-        .setFooter(`Rem 2.1`)
+        .setFooter(infos.version, client.user.displayAvatarURL)
 
-    message.channel.send({embed: embed});
+    message.channel.send(ownerembed);
+
+    } else {
+        const embed = new Discord.RichEmbed()
+        .setTitle(`Help requested by **${message.author.username}**`)
+        .setURL('https://google.com/')
+        .setColor('#329FFE')
+        .setThumbnail(message.author.avatarURL)
+        .addField('Basics', `\`${cmds.Basics}\``)
+        .addField('Fun', `\`${cmds.Fun}\``)
+        .addField('Music', `\`${cmds.Music}\``)
+        .addField('Moderation', `\`${cmds.Moderation}\``)
+        .addField('Others', `\`${cmds.Others}\``)
+        .setTimestamp()
+        .setFooter(infos.version, client.user.displayAvatarURL)
+
+    message.channel.send(embed);
+    }
 }
