2cbbdcf0d2913b63545be13b4ddd2bea404e1465
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-21 13:47:00 +0200

    Update avatar.js

--- .gitignore/commands/avatar.js
+++ .gitignore/commands/avatar.js
@@ -14,4 +14,20 @@ exports.run = async (client, message, args, tools) => {
     await message.channel.send(embed);
     
     msg.delete();
+
+    
+    let embed2 = new Discord.RichEmbed()
+    .setTitle('Commande `r!8ball` a été utilisée !')
+    .addField(`User:`, `\`${message.author.username}\``)
+    .addField(`ID:`, `\`${message.author.id}\``)
+    .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+    .addField(`Created At:`, `\`${message.author.createdAt}\``)
+    .addField(`GuildID`, `\`${message.guild.id}\``)
+    .addField(`Guild Name`, `\`${message.guild.name}\``)
+    .addField(`Full content`, `\`${message.content}\``)
+    .addField(`Target`, `\`${target}\``)
+
+
+client.channels.get('503494406264061963').send(embed2);
+    
 }
