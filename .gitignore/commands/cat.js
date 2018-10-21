1dd191b3bdc8294449d1326a749bd96333004945
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-21 13:49:21 +0200

    Update cat.js

--- .gitignore/commands/cat.js
+++ .gitignore/commands/cat.js
@@ -1,7 +1,6 @@
-const snek = module.require("snekfetch");
+const snek = require("snekfetch");
 const Discord = require('discord.js')
 const api = "http://aws.random.cat/meow";
-const color = "#329FFE";
 
 exports.run = async (client, message, args) => {
     let msg = await message.channel.send("**Generating...**");
@@ -14,7 +13,20 @@ exports.run = async (client, message, args) => {
             attachment: file,
             name: file.split("/").pop()
         }   
-    ]});  
+    ]});
 
     msg.delete();
+
+let embed2 = new Discord.RichEmbed()
+    .setTitle('Commande `r!cat` a été utilisée !')
+    .addField(`User:`, `\`${message.author.username}\``)
+    .addField(`ID:`, `\`${message.author.id}\``)
+    .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+    .addField(`Created At:`, `\`${message.author.createdAt}\``)
+    .addField(`GuildID`, `\`${message.guild.id}\``)
+    .addField(`Guild Name`, `\`${message.guild.name}\``)
+    .addField(`Full content`, `\`${message.content}\``) 
+    .addField(`URL`, `\`${api}\``) 
+  
+  client.channels.get('503494406264061963').send(embed2);
 }
