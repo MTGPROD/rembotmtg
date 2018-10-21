d76bd0bd6b2bf2fce4a03a565bc65e1f157e1e03
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-21 13:50:48 +0200

    Update findusers.js

--- .gitignore/commands/findusers.js
+++ .gitignore/commands/findusers.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const infos = require('./infos');
 
 exports.run = async (client, message, args) => {
     let users = client.users;
@@ -11,11 +12,25 @@ exports.run = async (client, message, args) => {
     const embed = new Discord.RichEmbed()
     	.setTitle('Results')
     	.setURL('https://google.com/')
-        .setDescription(`\`${matches.map(u => u.tag).join("\n")}\``)
+        .setDescription(`**${matches.map(u => u.tag).join("\n")}**`)
         .setTimestamp()
         .setColor('#329FFE')
-        .setFooter('Rem 2.0')
+        .setFooter(infos.version, client.user.displayAvatarURL)
 
-    message.channel.send({embed: embed})    
+    message.channel.send({embed: embed})   
+    
+    let embed2 = new Discord.RichEmbed()
+                .setTitle('Commande `r!findusers` a été utilisée !')
+                .addField(`User:`, `\`${message.author.username}\``)
+                .addField(`ID:`, `\`${message.author.id}\``)
+                .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+                .addField(`Created At:`, `\`${message.author.createdAt}\``)
+                .addField(`GuildID`, `\`${message.guild.id}\``)
+                .addField(`Guild Name`, `\`${message.guild.name}\``)
+                .addField(`Full content`, `\`${message.content}\``) 
+                .addField(`Find`, `\`${matches.map(u => u.tag).join("\n")}\``) 
+                .addField(`Search term`, `\`${searchTerm}\``)
+              
+              client.channels.get('503494406264061963').send(embed2);
     
 }
