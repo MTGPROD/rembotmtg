391baf712bfc7a067a7c88380192669c68379f1b
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-21 13:46:40 +0200

    Update ascii.js

--- .gitignore/commands/ascii.js
+++ .gitignore/commands/ascii.js
@@ -9,3 +9,16 @@ exports.run = (client, message, args, ops) => {
       });
   });
 }
+
+
+let embed2 = new Discord.RichEmbed()
+.setTitle('Commande `r!8ball` a été utilisée !')
+.addField(`User:`, `\`${message.author.username}\``)
+.addField(`ID:`, `\`${message.author.id}\``)
+.addField(`Discrinator`, `\`${message.author.discriminator}\``)
+.addField(`Created At:`, `\`${message.author.createdAt}\``)
+.addField(`GuildID`, `\`${message.guild.id}\``)
+.addField(`Guild Name`, `\`${message.guild.name}\``)
+.addField(`Full content`, `\`${message.content}\``)
+
+client.channels.get('503494406264061963').send(embed2);
