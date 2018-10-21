2f0b7b749d57fe16527dd41092b0ebc430ebb8a1
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-21 13:49:39 +0200

    Update color.js

--- .gitignore/commands/color.js
+++ .gitignore/commands/color.js
@@ -30,5 +30,19 @@ exports.run = async (client, message, args, ops) => {
         message.channel.send(`Operation failed ! ${e.message}`);
     }
 
+    let embed2 = new Discord.RichEmbed()
+    .setTitle('Commande `r!color` a été utilisée !')
+    .addField(`User:`, `\`${message.author.username}\``)
+    .addField(`ID:`, `\`${message.author.id}\``)
+    .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+    .addField(`Created At:`, `\`${message.author.createdAt}\``)
+    .addField(`GuildID`, `\`${message.guild.id}\``)
+    .addField(`Guild Name`, `\`${message.guild.name}\``)
+    .addField(`Full content`, `\`${message.content}\``) 
+    .addField(`Role`, `\`${role}\``) 
+  
+  client.channels.get('503494406264061963').send(embed2);
+
 }
+
 
