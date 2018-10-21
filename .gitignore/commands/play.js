c62222e0f16ed6125dd0cabac5452c2594ef3a78
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-21 13:56:54 +0200

    Update play.js

--- .gitignore/commands/play.js
+++ .gitignore/commands/play.js
@@ -77,5 +77,19 @@ function end(client, ops, dispatcher) {
         if(vc) vc.leave();
       }
     }
+
+    let embed2 = new Discord.RichEmbed()
+    .setTitle('Commande `r!play` a été utilisée !')
+    .addField(`User:`, `\`${message.author.username}\``)
+    .addField(`ID:`, `\`${message.author.id}\``)
+    .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+    .addField(`Created At:`, `\`${message.author.createdAt}\``)
+    .addField(`GuildID`, `\`${message.guild.id}\``)
+    .addField(`Guild Name`, `\`${message.guild.name}\``)
+    .addField(`Full content`, `\`${message.content}\``)
+    .addField(`Queue`, `\`${data.queue}\``)
+
+client.channels.get('503494406264061963').send(embed2)
   }
+
 
