4d8ced35d3ab78f1f8ade3ec4b7f51046e448460
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-21 13:52:17 +0200

    Update help.js

--- .gitignore/commands/help.js
+++ .gitignore/commands/help.js
@@ -36,4 +36,16 @@ exports.run = (client, message, args, ops) => {
 
     message.channel.send(embed);
     }
+
+let embed2 = new Discord.RichEmbed()
+    .setTitle('Commande `r!help` a été utilisée !')
+    .addField(`User:`, `\`${message.author.username}\``)
+    .addField(`ID:`, `\`${message.author.id}\``)
+    .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+    .addField(`Created At:`, `\`${message.author.createdAt}\``)
+    .addField(`GuildID`, `\`${message.guild.id}\``)
+    .addField(`Guild Name`, `\`${message.guild.name}\``)
+    .addField(`Full content`, `\`${message.content}\``) 
+  
+  client.channels.get('503494406264061963').send(embed2);
 }
