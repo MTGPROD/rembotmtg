be4804fe79526156ca0f55c25c6124509a9971a9
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-21 13:47:46 +0200

    Update b64encode.js

--- .gitignore/commands/b64encode.js
+++ .gitignore/commands/b64encode.js
@@ -3,4 +3,20 @@ const base64 = require("js-base64").Base64;
 module.exports.run = async (bot, message, args) => {
     const b64Encoded = base64.encode(args.join(" "));
     message.channel.send(`\`\`\`\n${b64Encoded}\`\`\``);
+
+    
+    let embed2 = new Discord.RichEmbed()
+    .setTitle('Commande `r!b64encode` a été utilisée !')
+    .addField(`User:`, `\`${message.author.username}\``)
+    .addField(`ID:`, `\`${message.author.id}\``)
+    .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+    .addField(`Created At:`, `\`${message.author.createdAt}\``)
+    .addField(`GuildID`, `\`${message.guild.id}\``)
+    .addField(`Guild Name`, `\`${message.guild.name}\``)
+    .addField(`Full content`, `\`${message.content}\``)
+    .addField(`Output`, `\`${b64Encoded}\``)
+
+
+client.channels.get('503494406264061963').send(embed2);
+    
 }
