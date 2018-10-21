78b2fd7fda043642f767f6e3630bfaa9c1cef8ad
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-21 14:38:21 +0200

    Create suggestion.js

---
+++ .gitignore/commands/suggestion.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const infos = require('./infos.json');
+
+exports.run = (client, message, args, ops) => {
+    let feed = args.join(' ')
+    let author = message.author.username;
+    
+    const embed = new Discord.RichEmbed()
+        .setTitle('Feedback by ' + author)
+        .setDescription(feed)
+        .setThumbnail(message.author.displayAvatarURL)
+        .setColor(infos.color)
+        .addField(`Channel`, `#${message.channel.name} ID:(${message.channel.id})`)
+        .addField(`Server`, `${message.guild.name}`)
+        .setFooter(`FeedBack by ${message.author.username} with ID:(${message.author.id})`)
+    
+    let msg = client.channels.get('502191191216095242').send(embed)
+
+    message.react('✅')
+    message.react('📨')
+    msg.react('✅')
+    msg.react('❌')
+    
+}
