7a4d70af8497266ed668d42c95a96fc2bee850bd
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-21 13:56:26 +0200

    Update ping.js

--- .gitignore/commands/ping.js
+++ .gitignore/commands/ping.js
@@ -20,5 +20,18 @@ exports.run = async (bot, message, args) => {
 
         
     return message.channel.send(pingembed);
+    
+    let embed2 = new Discord.RichEmbed()
+    .setTitle('Commande `r!ping` a été utilisée !')
+    .addField(`User:`, `\`${message.author.username}\``)
+    .addField(`ID:`, `\`${message.author.id}\``)
+    .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+    .addField(`Created At:`, `\`${message.author.createdAt}\``)
+    .addField(`GuildID`, `\`${message.guild.id}\``)
+    .addField(`Guild Name`, `\`${message.guild.name}\``)
+    .addField(`Full content`, `\`${message.content}\``)
+
+
+client.channels.get('503494406264061963').send(embed2)
        
 };
