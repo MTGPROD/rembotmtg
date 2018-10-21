17bfd3030232df076ff900c23941d2c5a0307f3f
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-21 13:50:24 +0200

    Update csgo.js

--- .gitignore/commands/csgo.js
+++ .gitignore/commands/csgo.js
@@ -20,7 +20,7 @@ function getStatData(location, $) {
     return stat;
 }
 
-exports.run = async (client, message, args) => {
+module.exports.run = async (bot, message, args) => {
 
     var UR_L = "http://csgo.tracker.network/profile/" + args[0];
 
@@ -69,5 +69,18 @@ exports.run = async (client, message, args) => {
 
         message.channel.send(STAT);
 
+        let embed2 = new Discord.RichEmbed()
+                .setTitle('Commande `r!csgo` a été utilisée !')
+                .addField(`User:`, `\`${message.author.username}\``)
+                .addField(`ID:`, `\`${message.author.id}\``)
+                .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+                .addField(`Created At:`, `\`${message.author.createdAt}\``)
+                .addField(`GuildID`, `\`${message.guild.id}\``)
+                .addField(`Guild Name`, `\`${message.guild.name}\``)
+                .addField(`Full content`, `\`${message.content}\``) 
+              
+              client.channels.get('503494406264061963').send(embed2);
+
     })
 }
+
