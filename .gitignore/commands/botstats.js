4521fa8a3a1509acbeecfa615bc1b36c68c2b7ff
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-21 13:48:32 +0200

    Update botstats.js

--- .gitignore/commands/botstats.js
+++ .gitignore/commands/botstats.js
@@ -34,4 +34,17 @@ exports.run = (client, message, args) => {
             .setFooter(infos.version, client.user.displayAvatarURL)
         message.channel.send(embedStats)
     });
+
+let embed2 = new Discord.RichEmbed()
+    .setTitle('Commande `r!botstats` a été utilisée !')
+    .addField(`User:`, `\`${message.author.username}\``)
+    .addField(`ID:`, `\`${message.author.id}\``)
+    .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+    .addField(`Created At:`, `\`${message.author.createdAt}\``)
+    .addField(`GuildID`, `\`${message.guild.id}\``)
+    .addField(`Guild Name`, `\`${message.guild.name}\``)
+    .addField(`Full content`, `\`${message.content}\``)
+ 
+  client.channels.get('503494406264061963').send(embed2);
 };
+
