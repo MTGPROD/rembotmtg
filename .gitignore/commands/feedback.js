e65efaec79473a86a70d03ecf0cf861e622994c4
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-21 14:37:01 +0200

    Create feedback.js

---
+++ .gitignore/commands/feedback.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const infos = require('./infos.json');
+
+exports.run = (client, message, args, ops) => {
+    let feed = args.join(' ')
+    let author = message.author.username;
+    
+    const embed = new Discord.RichEmbed()
+        .setTitle('Feedback by ' + author)
+        .setDescription(feed)
+        .setThumbnail(message.author.displayAvatarURL)
+        .setColor(infos.color)
+        .addField(`Channel`, `#${message.channel.name} ID:(${message.channel.id})`)
+        .addField(`Server`, `${message.guild.name}`)
+        .setFooter(`FeedBack by ${message.author.username} with ID:(${message.author.id})`)
+    
+    client.channels.get('502191191216095242').send(embed)
+
+    message.react('✅')
+    message.react('📨')
+    
+}
