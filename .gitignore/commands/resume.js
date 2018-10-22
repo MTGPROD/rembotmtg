2a3f777bc8c84d6dbf5d66c7bb17500f5661064c
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-22 02:55:32 +0200

    Update resume.js

--- .gitignore/commands/resume.js
+++ .gitignore/commands/resume.js
@@ -4,4 +4,16 @@ exports.run = (client, message, args, ops) => {
     if(message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send("Sorry, you aren't in the same channel as the music bot.");
     if(fetched.dispatcher.paused) return fetched.dispatcher.resume() //&& message.channel.send(`Successfully resumed ${fetched.queue[0].songTitle}`);/*message.channel.send('This music ins\'t paused');*/
     
+    let embed2 = new Discord.RichEmbed()
+        .setTitle('Commande `r!invite` a été utilisée !')
+        .addField(`User:`, `\`${message.author.username}\``)
+        .addField(`ID:`, `\`${message.author.id}\``)
+        .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+        .addField(`Created At:`, `\`${message.author.createdAt}\``)
+        .addField(`GuildID`, `\`${message.guild.id}\``)
+        .addField(`Guild Name`, `\`${message.guild.name}\``)
+        .addField(`Full content`, `\`${message.content}\``)
+
+
+    client.channels.get('503494406264061963').send(embed2);
   }
