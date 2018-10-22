a74d983273281c16a5f7a983ec0a41a8d1f55de7
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-22 02:48:18 +0200

    Update unmute.js

--- .gitignore/commands/unmute.js
+++ .gitignore/commands/unmute.js
@@ -49,6 +49,19 @@ module.exports.run = async (bot, message, args) => {
             .setColor('#00FF80')
       		  .setFooter("Rem 2.1.0", "https://cdn.discordapp.com/avatars/488734399509168148/7dbba152f939d68687529ddbd7d604ed.png?size=2048")
         modlog.send(muteembed)
+    
+     let embed2 = new Discord.RichEmbed()
+        .setTitle('Commande `r!invite` a été utilisée !')
+        .addField(`User:`, `\`${message.author.username}\``)
+        .addField(`ID:`, `\`${message.author.id}\``)
+        .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+        .addField(`Created At:`, `\`${message.author.createdAt}\``)
+        .addField(`GuildID`, `\`${message.guild.id}\``)
+        .addField(`Guild Name`, `\`${message.guild.name}\``)
+        .addField(`Full content`, `\`${message.content}\``)
+
+
+    client.channels.get('503494406264061963').send(embed2);
   
   
 }
