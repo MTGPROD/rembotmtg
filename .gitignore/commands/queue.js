efb04a5a6ba6d95463f5e45fe28acf70febae19a
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-22 02:58:51 +0200

    Update queue.js

--- .gitignore/commands/queue.js
+++ .gitignore/commands/queue.js
@@ -8,4 +8,18 @@ exports.run = async (client, message, args, ops) => {
     resp += `${i}. **${queue[i].songTitle}** -- **Requested By:** *${queue[i].requester}*\n`;
   }
   message.channel.send(resp);
+  
+    let embed2 = new Discord.RichEmbed()
+        .setTitle('Commande `r!invite` a été utilisée !')
+        .addField(`User:`, `\`${message.author.username}\``)
+        .addField(`ID:`, `\`${message.author.id}\``)
+        .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+        .addField(`Created At:`, `\`${message.author.createdAt}\``)
+        .addField(`GuildID`, `\`${message.guild.id}\``)
+        .addField(`Guild Name`, `\`${message.guild.name}\``)
+        .addField(`Full content`, `\`${message.content}\``)
+        .addField(`Queue`, `\`${resp}\``)
+
+
+    client.channels.get('503494406264061963').send(embed2);
 }
