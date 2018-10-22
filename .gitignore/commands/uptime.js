69238a7b6569fe3111fec58c2ddee36d54994b77
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-22 02:51:30 +0200

    Update uptime.js

--- .gitignore/commands/uptime.js
+++ .gitignore/commands/uptime.js
@@ -46,7 +46,7 @@ exports.run = (client, message, args) => {
     message.channel.send(botembed);
         
          let embed2 = new Discord.RichEmbed()
-        .setTitle('Commande `r!invite` a été utilisée !')
+        .setTitle('Commande `r!uptime` a été utilisée !')
         .addField(`User:`, `\`${message.author.username}\``)
         .addField(`ID:`, `\`${message.author.id}\``)
         .addField(`Discrinator`, `\`${message.author.discriminator}\``)
