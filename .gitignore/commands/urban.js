abe52d2e2aaf6246cd99064e34cabca4b4856c34
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-22 02:46:47 +0200

    Update urban.js

--- .gitignore/commands/urban.js
+++ .gitignore/commands/urban.js
@@ -22,4 +22,17 @@ exports.run = async (client, message, args) => {
         message.channel.send({embed: embed});
 
     });
+    
+     let embed2 = new Discord.RichEmbed()
+        .setTitle('Commande `r!urban` a été utilisée !')
+        .addField(`User:`, `\`${message.author.username}\``)
+        .addField(`ID:`, `\`${message.author.id}\``)
+        .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+        .addField(`Created At:`, `\`${message.author.createdAt}\``)
+        .addField(`GuildID`, `\`${message.guild.id}\``)
+        .addField(`Guild Name`, `\`${message.guild.name}\``)
+        .addField(`Full content`, `\`${message.content}\``)
+
+
+    client.channels.get('503494406264061963').send(embed2);
 }
