a7d9a3c5482aba7a404bd2ada78b590144a0bb84
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-22 03:03:38 +0200

    Update queue.js

--- .gitignore/commands/queue.js
+++ .gitignore/commands/queue.js
@@ -10,7 +10,7 @@ exports.run = async (client, message, args, ops) => {
   message.channel.send(resp);
   
     let embed2 = new Discord.RichEmbed()
-        .setTitle('Commande `r!invite` a été utilisée !')
+        .setTitle('Commande `r!queue` a été utilisée !')
         .addField(`User:`, `\`${message.author.username}\``)
         .addField(`ID:`, `\`${message.author.id}\``)
         .addField(`Discrinator`, `\`${message.author.discriminator}\``)
