6bebb1fc82f0c59232c42e6672a3b737109a9678
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-22 02:44:38 +0200

    Update ascii.js

--- .gitignore/commands/ascii.js
+++ .gitignore/commands/ascii.js
@@ -3,7 +3,7 @@ const ascii = require('ascii-art');
 exports.run = (client, message, args, ops) => {
   ascii.font(args.join(' '), 'Doom', function(rendered) {
   rendered = rendered.trimRight();
-  if(rendered.length > 2000) return message.channel.send('Sorry, that message is too long !');
+  //if(rendered.length > 2000) return message.channel.send('Sorry, that message is too long !');
   message.channel.send(rendered, {
     code: 'md'
       });
