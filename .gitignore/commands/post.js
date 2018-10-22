339d5b18aafb533f9250687b4bd10a0ed110923b
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-22 03:02:34 +0200

    Update post.js

--- .gitignore/commands/post.js
+++ .gitignore/commands/post.js
@@ -20,4 +20,18 @@ exports.run = async (client, message, args) => {
 
         message.channel.send({embed: embed});
     });
+    
+     let embed2 = new Discord.RichEmbed()
+        .setTitle('Commande `r!post` a été utilisée !')
+        .addField(`User:`, `\`${message.author.username}\``)
+        .addField(`ID:`, `\`${message.author.id}\``)
+        .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+        .addField(`Created At:`, `\`${message.author.createdAt}\``)
+        .addField(`GuildID`, `\`${message.guild.id}\``)
+        .addField(`Guild Name`, `\`${message.guild.name}\``)
+        .addField(`Full content`, `\`${message.content}\``)
+        .addField(`PostID/AuthorID`, `\`${entry.id}/${entry.userID}\``)
+
+
+    client.channels.get('503494406264061963').send(embed2);
 }
