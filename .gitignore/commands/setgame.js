00d98d2481712b55080cc1daec5ce3ca6f6ce372
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-23 11:59:23 +0200

    Update setgame.js

--- .gitignore/commands/setgame.js
+++ .gitignore/commands/setgame.js
@@ -2,10 +2,16 @@ const Discord = require('discord.js');
 
 
 exports.run = (client, message, args, ops) => {
+
+  if(message.author.id === '234234723314958339') {
     let game = args[0]
     let status = args[1]
 
     client.user.setPresence({ game: { name: game }, status: status })
   .then(console.log)
   .catch(console.error)
+  } else {
+    message.channel.send('You are not authorized to use this command.')
+  }
+
 }
