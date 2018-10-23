8e3e9502284b0242d1706e8a9d5746b73f428d06
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-23 03:10:17 +0200

    Delete shard.js

--- .gitignore/shard.js
+++
@@ -1,9 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./server.js', {
-  token: process.env.TOKEN,
-  autoSpawn: true
-});
-
-manager.spawn(2);
-
-manager.on('launch', shard => console.log(`[SHARD] Shard ${shard.id}/${manager.totalShards}`));
