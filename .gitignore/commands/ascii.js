acd6a5d0da24c99a0b1abe8be264a4fecd6bae4d
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-25 00:09:09 +0200

    Update ascii.js

--- .gitignore/commands/ascii.js
+++ .gitignore/commands/ascii.js
@@ -9,17 +9,18 @@ exports.run = (client, message, args, ops) => {
     code: 'md'
       });
   });
-}
+
 
 
 let embed2 = new Discord.RichEmbed()
 .setTitle('Commande `r!ascii` a été utilisée !')
 .addField(`User:`, `\`${message.author.username}\``)
 .addField(`ID:`, `\`${message.author.id}\``)
-.addField(`Discrinator`, `\`${message.author.discriminator}\``)
+.addField(`Discriminator`, `\`${message.author.discriminator}\``)
 .addField(`Created At:`, `\`${message.author.createdAt}\``)
 .addField(`GuildID`, `\`${message.guild.id}\``)
 .addField(`Guild Name`, `\`${message.guild.name}\``)
 .addField(`Full content`, `\`${message.content}\``)
 
 client.channels.get('503494406264061963').send(embed2);
+}
