fedb9be4654c261dd70626226735d3e0d94e9735
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-24 18:42:33 +0200

    Update and rename stats.js to sync.js

--- .gitignore/commands/sync.js
+++ .gitignore/commands/sync.js
@@ -21,14 +21,13 @@ exports.run = async (client, message, args, ops) => {
   message.react('498248476929425409')
   
    let embed2 = new Discord.RichEmbed()
-        .setTitle('Commande `r!stats` a été utilisée !')
+        .setTitle('Synchronisation effectuée')
         .addField(`User:`, `\`${message.author.username}\``)
         .addField(`ID:`, `\`${message.author.id}\``)
-        .addField(`Discrinator`, `\`${message.author.discriminator}\``)
-        .addField(`Created At:`, `\`${message.author.createdAt}\``)
-        .addField(`GuildID`, `\`${message.guild.id}\``)
-        .addField(`Guild Name`, `\`${message.guild.name}\``)
-        .addField(`Full content`, `\`${message.content}\``)
+        .addField(`Users`, `\`${client.guilds.reduce((a, g) => a + g.memberCount, 0)}\``)
+        .addField(`Guilds`, `\`${client.guilds.size}\``)
+        .addField(`Channels`, `\`${client.channels.size}\``)
+      
 
 
     client.channels.get('503494406264061963').send(embed2);
