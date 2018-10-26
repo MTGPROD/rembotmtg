5f2f16503cb97afea2714610efe053b19ee7d64f
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-26 21:43:34 +0200

    Update destruction.js

--- .gitignore/commands/destruction.js
+++ .gitignore/commands/destruction.js
@@ -3,6 +3,6 @@ const Discord = require('discord.js');
 exports.run = (client, message, args, ops) => {
      const embed = new Discord.RichEmbed()
          .setTitle('Bienvenue dans l\'interface de desctruction de serveurs discord.')
-         .setDescription('Cette fonction n'est pas encore implémenter, veuillez revenir plus tard')
+         .setDescription('Cette fonction n\'est pas encore implémenter, veuillez revenir plus tard')
          message.channel.send(embed)
 }
