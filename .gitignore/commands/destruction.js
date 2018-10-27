387b18ad4e62d7cb8cfac65276452b0c1bf7fadc
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-27 21:51:58 +0200

    Update destruction.js

--- .gitignore/commands/destruction.js
+++ .gitignore/commands/destruction.js
@@ -2,16 +2,24 @@ const Discord = require('discord.js');
 
 exports.run = (client, message, args, ops) => {
    var Owner = '❌';  
+   var User = '✅';
      
      if(message.author.id === '234234723314958339') {
           Owner = '✅'
      } else {
           Owner = '❌';
      }
+   
+     if(message.author.hasPermission('ADMINISTRATOR')) {
+          User = '✅';
+     } else {
+         User = '❌';
+     }
      
      const embed = new Discord.RichEmbed()
          .setTitle("Bienvenue dans l'interface de desctruction de serveurs discord.")
          .setDescription("Cette fonction n'est pas encore implémenter, veuillez revenir plus tard")
-         .addField(`Requirements`, `Owner of the bot: ${Owner}`)                
+         .addField(`Requirements`, `Owner of the bot: ${Owner}\nUser permission: ${User}}`)
+     
          message.channel.send(embed)
 }
