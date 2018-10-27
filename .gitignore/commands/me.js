772efe71c6a662559cfab5affbff1d31f92d21aa
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-27 22:16:20 +0200

    Update me.js

--- .gitignore/commands/me.js
+++ .gitignore/commands/me.js
@@ -1,12 +1,14 @@
 const Discord = require("discord.js");
 const infos = require('./infos.json');
 
-exports.run = async (client, message, args) => {
-    let target = message.author;
+exports.run = async (client, message, args, ops) => {
+    let target = message.mentions.users.first() || message.author;
     
     const embed = new Discord.RichEmbed()
         .setTitle(`Profile of ${target}`)
-        .addField(`Name`, `${target.username}`)
-
+        .addField(`Name:`, `${target.username}`)
+        .addField(`ID:`, `${target.id}`)
+        
+        .addField(`Discriminator`, `#${target.discriminator}`)
     message.channel.send(embed);
 }
