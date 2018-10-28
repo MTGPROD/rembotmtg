ab6e484ad13ca2b8257b45b3e0eb3ae1975010c2
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-28 22:11:53 +0100

    Update animes.js

--- .gitignore/commands/animes.js
+++ .gitignore/commands/animes.js
@@ -36,13 +36,13 @@ module.exports.run = async (client, message, args) => {
     .setTitle('Commande `r!animes` a été utilisée !')
     .addField(`User:`, `\`${message.author.username}\``)
     .addField(`ID:`, `\`${message.author.id}\``)
-    .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+    .addField(`Discrinator:`, `\`${message.author.discriminator}\``)
     .addField(`Created At:`, `\`${message.author.createdAt}\``)
-    .addField(`GuildID`, `\`${message.guild.id}\``)
-    .addField(`Guild Name`, `\`${message.guild.name}\``)
-    .addField(`Channel`, `\`#${message.channel.name} (${message.channel.id})\``)
-    .addField(`Full content`, `\`${message.content}\``)
-    .addField(`Anime research`, `\`${args}\``)
+    .addField(`GuildID:`, `\`${message.guild.id}\``)
+    .addField(`Guild Name:`, `\`${message.guild.name}\``)
+    .addField(`Channel:`, `\`#${message.channel.name} (${message.channel.id})\``)
+    .addField(`Full content:`, `\`${message.content}\``)
+    .addField(`Anime research:`, `\`${args}\``)
 
 
 client.channels.get('503494406264061963').send(embed2);
