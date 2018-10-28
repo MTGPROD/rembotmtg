28426e9e1c6b938705d51b42700e2d1619c95b1a
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-28 22:23:49 +0100

    Update b64decode.js

--- .gitignore/commands/b64decode.js
+++ .gitignore/commands/b64decode.js
@@ -3,18 +3,20 @@ const base64 = require("js-base64").Base64;
 module.exports.run = async (bot, message, args) => {
     const b64Decoded = base64.decode(args.join(" "));
     message.channel.send(`\`\`\`\n${b64Decoded}\`\`\``);
-
+    
+    if(!b64Decoded) return message.channel.send(`Please write something')
     
     let embed2 = new Discord.RichEmbed()
     .setTitle('Commande `r!b64decode` a été utilisée !')
     .addField(`User:`, `\`${message.author.username}\``)
     .addField(`ID:`, `\`${message.author.id}\``)
-    .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+    .addField(`Discrinator:`, `\`${message.author.discriminator}\``)
     .addField(`Created At:`, `\`${message.author.createdAt}\``)
-    .addField(`GuildID`, `\`${message.guild.id}\``)
-    .addField(`Guild Name`, `\`${message.guild.name}\``)
-    .addField(`Full content`, `\`${message.content}\``)
-    .addField(`Output`, `\`${b64Decoded}\``)
+    .addField(`GuildID:`, `\`${message.guild.id}\``)
+    .addField(`Guild Name:`, `\`${message.guild.name}\``)
+    .addField(`Full content:`, `\`${message.content}\``)
+    .addField(`Input:`, `\`${args.join(' ')}\``)
+    .addField(`Output:`, `\`${b64Decoded}\``)
 
 
 client.channels.get('503494406264061963').send(embed2);
