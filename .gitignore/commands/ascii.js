1871a0988cf01bd84e0d93d114f678b11d641048
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-28 22:16:56 +0100

    Update ascii.js

--- .gitignore/commands/ascii.js
+++ .gitignore/commands/ascii.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 exports.run = (client, message, args, ops) => {
   ascii.font(args.join(' '), 'Doom', function(rendered) {
   rendered = rendered.trimRight();
-  if(rendered.length > 1800) return message.channel.send('Sorry, that message is too long !');
+  if(rendered.length > 1950) return message.channel.send('Sorry, that message is too long !');
   message.channel.send(rendered, {
     code: 'md'
       });
@@ -16,11 +16,13 @@ let embed2 = new Discord.RichEmbed()
 .setTitle('Commande `r!ascii` a été utilisée !')
 .addField(`User:`, `\`${message.author.username}\``)
 .addField(`ID:`, `\`${message.author.id}\``)
-.addField(`Discriminator`, `\`${message.author.discriminator}\``)
+.addField(`Discriminator:`, `\`${message.author.discriminator}\``)
 .addField(`Created At:`, `\`${message.author.createdAt}\``)
-.addField(`GuildID`, `\`${message.guild.id}\``)
-.addField(`Guild Name`, `\`${message.guild.name}\``)
-.addField(`Full content`, `\`${message.content}\``)
+.addField(`GuildID:`, `\`${message.guild.id}\``)
+.addField(`Guild Name:`, `\`${message.guild.name}\``)
+.addField(`Full content:`, `\`${message.content}\``)
+.addField(`Channel:`, `\`#${message.channel.name} (${message.channel.id})\``)
+.setThumbnail(message.author.avatarURL)
 
 client.channels.get('503494406264061963').send(embed2);
 }
