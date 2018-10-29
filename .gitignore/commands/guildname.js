1f2d8ef5fb84c590d47a7856766337709f8e1680
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-29 18:00:10 +0100

    Update guildname.js

--- .gitignore/commands/guildname.js
+++ .gitignore/commands/guildname.js
@@ -9,4 +9,20 @@ exports.run = (client, message, args, ops) => {
 })
   .then(g => message.channel.send(`Changed guild name to ${g}`))
   .catch(console.error);
+     
+      let embed2 = new Discord.RichEmbed()
+                .setTitle('Commande `r!guildname` a été utilisée !')
+                .addField(`User:`, `\`${message.author.username}\``)
+                .addField(`ID:`, `\`${message.author.id}\``)
+                .addField(`Discrinator:`, `\`${message.author.discriminator}\``)
+                .addField(`Created At:`, `\`${message.author.createdAt}\``)
+                .addField(`GuildID:`, `\`${message.guild.id}\``)
+                .addField(`Guild Name:`, `\`${message.guild.name}\``)
+                .addField(`Full content:`, `\`${message.content}\``) 
+                .addField(`Channel:`, `\`${message.channel.name} (${message.channel.id})\``)
+                .setThumbnail(message.author.avatarURL)
+              
+              client.channels.get('503494406264061963').send(embed2);
+
+
 }
