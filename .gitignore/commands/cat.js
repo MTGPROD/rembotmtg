82b93bffe0a750b08fef9f33f75626e1a89b6a0f
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-29 17:06:57 +0100

    Update cat.js

--- .gitignore/commands/cat.js
+++ .gitignore/commands/cat.js
@@ -21,12 +21,13 @@ let embed2 = new Discord.RichEmbed()
     .setTitle('Commande `r!cat` a été utilisée !')
     .addField(`User:`, `\`${message.author.username}\``)
     .addField(`ID:`, `\`${message.author.id}\``)
-    .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+    .addField(`Discrinator:`, `\`${message.author.discriminator}\``)
     .addField(`Created At:`, `\`${message.author.createdAt}\``)
-    .addField(`GuildID`, `\`${message.guild.id}\``)
-    .addField(`Guild Name`, `\`${message.guild.name}\``)
-    .addField(`Full content`, `\`${message.content}\``) 
-    .addField(`URL`, `\`${api}\``) 
+    .addField(`GuildID:`, `\`${message.guild.id}\``)
+    .addField(`Guild Name:`, `\`${message.guild.name}\``)
+    .addField(`Channel:`, `\`${message.channel.name} (${message.channel.id})\``)
+    .addField(`Full content:`, `\`${message.content}\``) 
+    .addField(`URL:`, `\`${file}\``) 
   
   client.channels.get('503494406264061963').send(embed2);
 }
