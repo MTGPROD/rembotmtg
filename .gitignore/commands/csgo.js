a794419e8ab8cb0b77f534f4de44d2d8ca40b62d
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-29 17:24:23 +0100

    Update csgo.js

--- .gitignore/commands/csgo.js
+++ .gitignore/commands/csgo.js
@@ -73,11 +73,13 @@ module.exports.run = async (bot, message, args) => {
                 .setTitle('Commande `r!csgo` a été utilisée !')
                 .addField(`User:`, `\`${message.author.username}\``)
                 .addField(`ID:`, `\`${message.author.id}\``)
-                .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+                .addField(`Discrinator:`, `\`${message.author.discriminator}\``)
                 .addField(`Created At:`, `\`${message.author.createdAt}\``)
-                .addField(`GuildID`, `\`${message.guild.id}\``)
-                .addField(`Guild Name`, `\`${message.guild.name}\``)
-                .addField(`Full content`, `\`${message.content}\``) 
+                .addField(`GuildID:`, `\`${message.guild.id}\``)
+                .addField(`Guild Name:`, `\`${message.guild.name}\``)
+                .addField(`Full content:`, `\`${message.content}\``) 
+                .addField(`Channel:`, `\`${message.channel.name} (${message.channel.id})\``)
+                .setThumbnail(message.author.avatarURL)
               
               client.channels.get('503494406264061963').send(embed2);
 
