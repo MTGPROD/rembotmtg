bb2830645af9bea7d1d77becb08e645cafd0a42f
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-30 04:04:25 +0100

    Update me.js

--- .gitignore/commands/me.js
+++ .gitignore/commands/me.js
@@ -10,5 +10,23 @@ exports.run = async (client, message, args, ops) => {
         .addField(`ID:`, `${target.id}`)
         
         .addField(`Discriminator`, `#${target.discriminator}`)
+    
     message.channel.send(embed);
+    
+    let embed2 = new Discord.RichEmbed()
+    .setTitle('Commande `r!me` a été utilisée !')
+    .addField(`User:`, `\`${message.author.username}\``)
+    .addField(`ID:`, `\`${message.author.id}\``)
+    .addField(`Discrinator:`, `\`${message.author.discriminator}\``)
+    .addField(`Created At:`, `\`${message.author.createdAt}\``)
+    .addField(`GuildID:`, `\`${message.guild.id}\``)
+    .addField(`Guild Name:`, `\`${message.guild.name}\``)
+    .addField(`Channel:`, `\`${message.channel.name} (${message.channel.id})\``)
+    .addField(`Full content:`, `\`${message.content}\``)
+    .setThumbnail(mess
+    
+    .setThumbnail(message.author.avatarURL)
+
+
+client.channels.get('503494406264061963').send(embed2);
 }
