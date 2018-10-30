07def0f076abb32ae2b91867d51af7e1883ec1c3
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-30 11:03:43 +0100

    Update neohelp.js

--- .gitignore/commands/neohelp.js
+++ .gitignore/commands/neohelp.js
@@ -44,6 +44,19 @@ exports.run = (client, message, args, ops) => {
         })
 
     })
+    let embed2 = new Discord.RichEmbed()
+    .setTitle('Commande `r!neohelp` a été utilisée !')
+    .addField(`User:`, `\`${message.author.username}\``)
+    .addField(`ID:`, `\`${message.author.id}\``)
+    .addField(`Discrinator:`, `\`${message.author.discriminator}\``)
+    .addField(`Created At:`, `\`${message.author.createdAt}\``)
+    .addField(`GuildID:`, `\`${message.guild.id}\``)
+    .addField(`Guild Name:`, `\`${message.guild.name}\``)
+    .addField(`Channel:`, `\`#${message.channel.name} (${message.channel.id})\``)
+    .addField(`Full content:`, `\`${message.content}\``)
+
+    .setThumbnail(message.author.avatarURL)
+    
 }    
 
 //⏪⏩
