aa9bb4240ea99751957b32d9738c050072285d6c
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-30 03:58:01 +0100

    Update leave.js

--- .gitignore/commands/leave.js
+++ .gitignore/commands/leave.js
@@ -4,4 +4,20 @@ exports.run = (client, message, args, ops) => {
     if(message.guild.me.voiceChannelID !== message.member.voiceChannelID) return message.channel.send('You aren\'t connected to the same channel')
     message.guild.me.voiceChannel.leave()
     message.channel.send('Leaving channel...')
+    
+    let embed2 = new Discord.RichEmbed()
+    .setTitle('Commande `r!leave` a été utilisée !')
+    .addField(`User:`, `\`${message.author.username}\``)
+    .addField(`ID:`, `\`${message.author.id}\``)
+    .addField(`Discrinator:`, `\`${message.author.discriminator}\``)
+    .addField(`Created At:`, `\`${message.author.createdAt}\``)
+    .addField(`GuildID:`, `\`${message.guild.id}\``)
+    .addField(`Guild Name:`, `\`${message.guild.name}\``)
+    .addField(`Channel:`, `\`${message.channel.name} (${message.channel.id})\``)
+    .addField(`Full content:`, `\`${message.content}\``)
+    
+    .setThumbnail(message.author.avatarURL)
+
+
+client.channels.get('503494406264061963').send(embed2);
 }
