41c567c0091b6970539794846328c81e5f9c715f
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-10-31 22:12:24 +0100

    Update newrole.js

--- .gitignore/commands/newrole.js
+++ .gitignore/commands/newrole.js
@@ -30,14 +30,15 @@ exports.run = (client, message, args, ops) => {
         .addField(`position:`, `${position}`)
         
     let embed2 = new Discord.RichEmbed()
-        .setTitle('Commande `r!invite` a été utilisée !')
+        .setTitle('Commande `r!newrole` a été utilisée !')
         .addField(`User:`, `\`${message.author.username}\``)
         .addField(`ID:`, `\`${message.author.id}\``)
-        .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+        .addField(`Discrinator:`, `\`${message.author.discriminator}\``)
         .addField(`Created At:`, `\`${message.author.createdAt}\``)
-        .addField(`GuildID`, `\`${message.guild.id}\``)
-        .addField(`Guild Name`, `\`${message.guild.name}\``)
-        .addField(`Full content`, `\`${message.content}\``)
+        .addField(`GuildID:`, `\`${message.guild.id}\``)
+        .addField(`Guild Name:`, `\`${message.guild.name}\``)
+        .addField(`Channel`, `\`${message}\``)
+        .addField(`Full content:`, `\`${message.content}\``)
 
 
     client.channels.get('503494406264061963').send(embed2);
