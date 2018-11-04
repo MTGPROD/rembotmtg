941dcd3c972e7a5495ff12831e085be14e8aef8d
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-11-04 14:26:42 +0100

    Update dashboard.js

--- .gitignore/commands/dashboard.js
+++ .gitignore/commands/dashboard.js
@@ -40,4 +40,17 @@ exports.run = (client, message, args) => {
             
  })       
 })
+    let embed2 = new Discord.RichEmbed()
+    .setTitle('Commande `r!dashboard` a été utilisée !')
+    .addField(`User:`, `\`${message.author.username}\``)
+    .addField(`ID:`, `\`${message.author.id}\``)
+    .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+    .addField(`Created At:`, `\`${message.author.createdAt}\``)
+    .addField(`GuildID:`, `\`${message.guild.id}\``)
+    .addField(`Guild Name:`, `\`${message.guild.name}\``)
+    .addField(`Channel:`, `\`#${message.channel.name} (${message.channel.id})\``)
+    .addField(`Full content:`, `\`${message.content}\``) 
+    .setThumbnail(message.author.avatarURL)
+  
+  client.channels.get('503494406264061963').send(embed2);
 }
