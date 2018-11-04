913412ad5f86039af8b4f0c9e507c693ac450e98
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-11-04 14:27:31 +0100

    Update documentation.js

--- .gitignore/commands/documentation.js
+++ .gitignore/commands/documentation.js
@@ -7,4 +7,18 @@ exports.run = async (client, message, args) => {
         .setDescription(`Please write the name of the command, you have the choice between all these commands:\n\n EX: \`r!docu avatar\` \n${list.Basics}\n${list.Fun}\n${list.Music}\n${list.Games}\n${list.Moderation}\n${list.Others}`)
 
     message.channel.send({embed: embed})
+   
+   let embed2 = new Discord.RichEmbed()
+    .setTitle('Commande `r!documentation` a été utilisée !')
+    .addField(`User:`, `\`${message.author.username}\``)
+    .addField(`ID:`, `\`${message.author.id}\``)
+    .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+    .addField(`Created At:`, `\`${message.author.createdAt}\``)
+    .addField(`GuildID:`, `\`${message.guild.id}\``)
+    .addField(`Guild Name:`, `\`${message.guild.name}\``)
+    .addField(`Channel:`, `\`#${message.channel.name} (${message.channel.id})\``)
+    .addField(`Full content:`, `\`${message.content}\``) 
+    .setThumbnail(message.author.avatarURL)
+  
+  client.channels.get('503494406264061963').send(embed2);
 };
