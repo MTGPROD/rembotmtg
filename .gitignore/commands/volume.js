12d792765aa553a068c996476cf14f69af330dfd
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-11-04 13:20:39 +0100

    Update volume.js

--- .gitignore/commands/volume.js
+++ .gitignore/commands/volume.js
@@ -4,17 +4,20 @@ exports.run = (client, message, args, ops) => {
   if(message.member.voiceChannel !== message.guild.me.voiceChannel) return message.chanel.send("Sorry, you currently aren't in the same channel as the bot.")
   if(isNaN(args[0]) || args[0] > 1000 || args[0] < 0) return message.channel.send('Please input a number between `0-1000`');
   fetched.dispatcher.setVolume(args[0]/100);
-  message.channel.send(`Successfully set the volume to \`${args[0]}\``)
+  message.channel.send(`Succesfuly set the volume to \`${args[0]}\``)
   
    let embed2 = new Discord.RichEmbed()
         .setTitle('Commande `r!volume` a été utilisée !')
         .addField(`User:`, `\`${message.author.username}\``)
         .addField(`ID:`, `\`${message.author.id}\``)
-        .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+        .addField(`Discrinator:`, `\`${message.author.discriminator}\``)
         .addField(`Created At:`, `\`${message.author.createdAt}\``)
-        .addField(`GuildID`, `\`${message.guild.id}\``)
-        .addField(`Guild Name`, `\`${message.guild.name}\``)
-        .addField(`Full content`, `\`${message.content}\``)
+        .addField(`GuildID:`, `\`${message.guild.id}\``)
+        .addField(`Guild Name:`, `\`${message.guild.name}\``)
+        .addField(`Channel:`, `\`#${message.channel.name} (${message.channel.id})\``)
+        .addField(`Full content:`, `\`${message.content}\``)
+        .addField(`Volume`, `\`${args[0]}\``)
+        .setThumbnail(message.author.displayAvatarURL)
 
 
     client.channels.get('503494406264061963').send(embed2);
