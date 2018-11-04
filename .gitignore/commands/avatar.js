4b428d96ce145732dfd34a93ebd4167e98ebc97d
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-11-04 13:11:38 +0100

    Update avatar.js

--- .gitignore/commands/avatar.js
+++ .gitignore/commands/avatar.js
@@ -1,19 +1,26 @@
+/*.setDescription(`[Link](${message.author.avatarURL})`)*/
 const Discord = require('discord.js');
 
 exports.run = async (client, message, args, tools) => {
-    let msg = await message.channel.send("**<a:Load:491296766835294208> Loading...**")
     let target = message.mentions.users.first() || message.author;
  
     const embed = new Discord.RichEmbed()
         .setTitle(`**${target.username}'s** avatar !`)
-        .setDescription(`[Link](${message.author.avatarURL})`)
-        .setColor('RANDOM')
-        .setImage(target.avatarURL)
+        .setDescription(`[**Link**](${message.author.avatarURL})`)
+        .setImage('https://cdn.discordapp.com/attachments/503494406264061963/508315779993829427/Double_Ring-1s-200px.gif')
         .setFooter(`Requested by ${message.author.username}`, message.author.displayAvatarURL)
 
-    await message.channel.send(embed);
-    
-    msg.delete();
+    await message.channel.send({embed: embed}).then(msg => {
+        let target = message.mentions.users.first() || message.author;
+        
+        setTimeout(function() {
+            embed.setURL('https://google.com')
+         embed.setColor('RANDOM')
+         embed.setImage(target.avatarURL)
+         msg.edit({embed: embed})
+
+        }, 1500)
+    })
 
     
     let embed2 = new Discord.RichEmbed()
@@ -24,12 +31,12 @@ exports.run = async (client, message, args, tools) => {
     .addField(`Created At:`, `\`${message.author.createdAt}\``)
     .addField(`GuildID:`, `\`${message.guild.id}\``)
     .addField(`Guild Name:`, `\`${message.guild.name}\``)
+    .addField(`Channel:`, `\`${message.channel.name} (${message.channel.id})\``)
     .addField(`Full content:`, `\`${message.content}\``)
-    .addField(`Channel:`, `\`#${message.channel.name} (${message.channel.id})\``)
     .addField(`Target:`, `\`${target}\``) 
-    .setThumbnail(target.avatarURL)
 
 
 client.channels.get('503494406264061963').send(embed2);
     
 }
+
