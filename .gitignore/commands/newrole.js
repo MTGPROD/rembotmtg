bcbaa7b811a5d2d63e94e2a55dca6bb1924cd220
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-11-04 15:03:00 +0100

    Update newrole.js

--- .gitignore/commands/newrole.js
+++ .gitignore/commands/newrole.js
@@ -28,18 +28,18 @@ exports.run = (client, message, args, ops) => {
         .addField(`Mentionable:`, `${mentionable}`)
         .addField(`Hoist:`, `${hoist}`)
         .addField(`position:`, `${position}`)
-        
-    let embed2 = new Discord.RichEmbed()
-        .setTitle('Commande `r!newrole` a été utilisée !')
-        .addField(`User:`, `\`${message.author.username}\``)
-        .addField(`ID:`, `\`${message.author.id}\``)
-        .addField(`Discrinator:`, `\`${message.author.discriminator}\``)
-        .addField(`Created At:`, `\`${message.author.createdAt}\``)
-        .addField(`GuildID:`, `\`${message.guild.id}\``)
-        .addField(`Guild Name:`, `\`${message.guild.name}\``)
-        .addField(`Channel`, `\`${message}\``)
-        .addField(`Full content:`, `\`${message.content}\``)
+     
+ let embed2 = new Discord.RichEmbed()
+    .setTitle('Commande `r!newrole` a été utilisée !')
+    .addField(`User:`, `\`${message.author.username}\``)
+    .addField(`ID:`, `\`${message.author.id}\``)
+    .addField(`Discrinator:`, `\`${message.author.discriminator}\``)
+    .addField(`Created At:`, `\`${message.author.createdAt}\``)
+    .addField(`GuildID:`, `\`${message.guild.id}\``)
+    .addField(`Guild Name:`, `\`${message.guild.name}\``)
+    .addField(`Channel:`, `\`#${message.channel.name} (${message.channel.id})\``)
+    .addField(`Full content:`, `\`${message.content}\``)
+    .setThumbnail(message.author.avatarURL)
 
-
-    client.channels.get('503494406264061963').send(embed2);
+client.channels.get('503494406264061963').send(embed2);
 }
