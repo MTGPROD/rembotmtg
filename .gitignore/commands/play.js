3858fe94986d84a0c45bebaeb9ed52f44805579b
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-11-04 15:16:04 +0100

    Update play.js

--- .gitignore/commands/play.js
+++ .gitignore/commands/play.js
@@ -78,18 +78,21 @@ function end(client, ops, dispatcher) {
       }
     }
 
-    let embed2 = new Discord.RichEmbed()
+   let embed2 = new Discord.RichEmbed()
     .setTitle('Commande `r!play` a été utilisée !')
     .addField(`User:`, `\`${message.author.username}\``)
     .addField(`ID:`, `\`${message.author.id}\``)
-    .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+    .addField(`Discrinator:`, `\`${message.author.discriminator}\``)
     .addField(`Created At:`, `\`${message.author.createdAt}\``)
-    .addField(`GuildID`, `\`${message.guild.id}\``)
-    .addField(`Guild Name`, `\`${message.guild.name}\``)
-    .addField(`Full content`, `\`${message.content}\``)
-    .addField(`Queue`, `\`${data.queue}\``)
+    .addField(`GuildID:`, `\`${message.guild.id}\``)
+    .addField(`Guild Name:`, `\`${message.guild.name}\``)
+    .addField(`Full content`:, `\`${message.content}\``)
+    .addField(`Queue:`, `\`${queue}\``)
+    .setThumbnail(message.author.displayAvatarURL)
 
-client.channels.get('503494406264061963').send(embed2)
+
+client.channels.get('503494406264061963').send(embed2);
+ 
   }
 
 
