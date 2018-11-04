074ba5234c707c20b37cde8918728398860aa750
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-11-04 14:26:03 +0100

    Update cpu.js

--- .gitignore/commands/cpu.js
+++ .gitignore/commands/cpu.js
@@ -24,5 +24,18 @@ exports.run = (client, message, args, ops) => {
         msg.delete(60000)
     })
   })
+    let embed2 = new Discord.RichEmbed()
+    .setTitle('Commande `r!cpu` a été utilisée !')
+    .addField(`User:`, `\`${message.author.username}\``)
+    .addField(`ID:`, `\`${message.author.id}\``)
+    .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+    .addField(`Created At:`, `\`${message.author.createdAt}\``)
+    .addField(`GuildID:`, `\`${message.guild.id}\``)
+    .addField(`Guild Name:`, `\`${message.guild.name}\``)
+    .addField(`Channel:`, `\`#${message.channel.name} (${message.channel.id})\``)
+    .addField(`Full content:`, `\`${message.content}\``) 
+    .setThumbnail(message.author.avatarURL)
+  
+  client.channels.get('503494406264061963').send(embed2);
 
 }
