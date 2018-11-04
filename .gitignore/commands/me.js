f5183a33e0445cfb1f79c52f638ad82e99ef2214
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-11-04 14:56:27 +0100

    Update me.js

--- .gitignore/commands/me.js
+++ .gitignore/commands/me.js
@@ -12,8 +12,8 @@ exports.run = async (client, message, args, ops) => {
         .addField(`Discriminator`, `#${target.discriminator}`)
     
     message.channel.send(embed);
-    
-    let embed2 = new Discord.RichEmbed()
+
+     let embed2 = new Discord.RichEmbed()
     .setTitle('Commande `r!me` a été utilisée !')
     .addField(`User:`, `\`${message.author.username}\``)
     .addField(`ID:`, `\`${message.author.id}\``)
@@ -21,12 +21,8 @@ exports.run = async (client, message, args, ops) => {
     .addField(`Created At:`, `\`${message.author.createdAt}\``)
     .addField(`GuildID:`, `\`${message.guild.id}\``)
     .addField(`Guild Name:`, `\`${message.guild.name}\``)
-    .addField(`Channel:`, `\`${message.channel.name} (${message.channel.id})\``)
-    .addField(`Full content/*66*/:`, `\`${message.content}\``)
-    
-    
+    .addField(`Channel:`, `\`#${message.channel.name} (${message.channel.id})\``)
+    .addField(`Full content:`, `\`${message.content}\``)
+    .addField(`"Kiss user":`, `\`${hugUser}\``)
     .setThumbnail(message.author.avatarURL)
-
-
-client.channels.get('503494406264061963').send(embed2);
 }
