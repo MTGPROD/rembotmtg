0258396da4b826b882b062e5e350cf89f07b1885
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-11-04 15:00:43 +0100

    Update newchannel.js

--- .gitignore/commands/newchannel.js
+++ .gitignore/commands/newchannel.js
@@ -7,26 +7,23 @@ exports.run = (client, message, args, ops) => {
     if(!name) return message.channel.send('Please input a name')
     if(!type) return message.channel.send('Please input a type `text/voice/category`')
 
-m
+
     message.guild.createChannel(name, type)
 
     message.channel.send('Channel created')
-    
-    
+   
         
-    let embed2 = new Discord.RichEmbed()
-    .setTitle('Commande `r!newchannel` a été utilisée !')
+ let embed2 = new Discord.RichEmbed()
+    .setTitle('Commande `r!me` a été utilisée !')
     .addField(`User:`, `\`${message.author.username}\``)
     .addField(`ID:`, `\`${message.author.id}\``)
-    .addField(`Discrinator`, `\`${message.author.discriminator}\``)
+    .addField(`Discrinator:`, `\`${message.author.discriminator}\``)
     .addField(`Created At:`, `\`${message.author.createdAt}\``)
-    .addField(`GuildID`, `\`${message.guild.id}\``)
-    .addField(`Guild Name`, `\`${message.guild.name}\``)
-    .addField(`Channel`, `\`#${message.channel.name} (${message.channel.id})\``)
-    .addField(`Full content`, `\`${message.content}\``)
-    
+    .addField(`GuildID:`, `\`${message.guild.id}\``)
+    .addField(`Guild Name:`, `\`${message.guild.name}\``)
+    .addField(`Channel:`, `\`#${message.channel.name} (${message.channel.id})\``)
+    .addField(`Full content:`, `\`${message.content}\``)
     .setThumbnail(message.author.avatarURL)
-
 
 client.channels.get('503494406264061963').send(embed2);
 }
