71b0e5dda45b0eb542819a4b6f0596a5d7aba73c
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-11-07 21:19:08 +0100

    Update cat.js

--- .gitignore/commands/cat.js
+++ .gitignore/commands/cat.js
@@ -8,26 +8,24 @@ exports.run = async (client, message, args) => {
     let file = (await snek.get(api)).body.file;
     if(!file) return message.channel.send("**I broke ! Try again.**");
 
-    await message.channel.send({files: [
-        {
-            attachment: file,
-            name: file.split("/").pop()
-        }   
-    ]});
+    const embed = new Discord.RichEmbed()
+        .setImage(file)
 
     msg.delete();
 
 let embed2 = new Discord.RichEmbed()
     .setTitle('Commande `r!cat` a été utilisée !')
+    .setColor('#36393F')
     .addField(`User:`, `\`${message.author.username}\``)
     .addField(`ID:`, `\`${message.author.id}\``)
     .addField(`Discrinator:`, `\`${message.author.discriminator}\``)
     .addField(`Created At:`, `\`${message.author.createdAt}\``)
     .addField(`GuildID:`, `\`${message.guild.id}\``)
     .addField(`Guild Name:`, `\`${message.guild.name}\``)
-    .addField(`Channel:`, `\`${message.channel.name} (${message.channel.id})\``)
+    .addField(`Channel:`, `\`#${message.channel.name} (${message.channel.id})\``)
     .addField(`Full content:`, `\`${message.content}\``) 
     .addField(`URL:`, `\`${file}\``) 
+    .setThumbnail(message.author.avatarURL)
   
   client.channels.get('503494406264061963').send(embed2);
 }
