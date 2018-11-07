cc474b63b71ec1e33d54c41205704d9bf4fcacd9
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-11-08 00:35:50 +0100

    Update setregion.js

--- .gitignore/commands/setregion.js
+++ .gitignore/commands/setregion.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 
 exports.run = (client, message, args, ops) => {
     let region = args.join(' ')
+    if(!region) message.channel.send('Usage: r!setregion japan/singapore/eu-central/us-central/london/eu-west/amsterdam/brazil/us-west/hongkong/us-south/southafrica/us-east/sydney/frankfurt/russia')
 
     message.guild.setRegion(region)
  .then(g => message.channel.send(`Updated guild region to ${g.region}`))
