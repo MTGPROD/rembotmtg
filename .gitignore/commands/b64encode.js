fc966f57f8d6dcaf0232dee7657ec1c88cc386b5
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-11-07 20:10:06 +0100

    Update b64encode.js

--- .gitignore/commands/b64encode.js
+++ .gitignore/commands/b64encode.js
@@ -2,13 +2,15 @@ const base64 = require("js-base64").Base64;
 const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
-    const b64Encoded = base64.encode(args.join(" "));
-    message.channel.send(`\`\`\`\n${b64Encoded}\`\`\``);
+    const b64Encoded = base64.encode(args.join(" "))
+    
+    if(!b64Encoded) return message.channel.send('Please write something') 
 
-    if(!b64Encoded) return message.channel.send('Please write something')
+    message.channel.send(`\`\`\`\n${b64Encoded}\`\`\``);
     
     let embed2 = new Discord.RichEmbed()
     .setTitle('Commande `r!b64encode` a été utilisée !')
+    .setColor('#36393F')
     .addField(`User:`, `\`${message.author.username}\``)
     .addField(`ID:`, `\`${message.author.id}\``)
     .addField(`Discrinator:`, `\`${message.author.discriminator}\``)
@@ -19,6 +21,7 @@ module.exports.run = async (bot, message, args) => {
     .addField(`Channel:`, `\`#${message.channel.name} (${message.channel.id})\``)
     .addField(`Input:`, `\`${args.join(' ')}\``)
     .addField(`Output:`, `\`${b64Encoded}\``)
+    .setThumbnail(message.author.avatarURL)
 
 
 client.channels.get('503494406264061963').send(embed2);
