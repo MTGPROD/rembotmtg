714982aad7ea37eabcba1429e9115e6b359ae3f8
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-11-08 00:12:06 +0100

    Update volume.js

--- .gitignore/commands/volume.js
+++ .gitignore/commands/volume.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args, ops) => {
   if(!fetched) return message.channel.send("There currently ins't any music playing in this guild !");
   if(message.member.voiceChannel !== message.guild.me.voiceChannel) return message.chanel.send("Sorry, you currently aren't in the same channel as the bot.")
   if(isNaN(args[0]) || args[0] > 1000 || args[0] < 0) return message.channel.send('Please input a number between `0-1000`');
-  fetched.dispatcher.setVolume(args[0]/100);
+  fetched.dispatcher.setVolume(args[0]/1000);
   message.channel.send(`Succesfuly set the volume to \`${args[0]}\``)
   
    let embed2 = new Discord.RichEmbed()
@@ -16,7 +16,7 @@ exports.run = (client, message, args, ops) => {
         .addField(`Guild Name:`, `\`${message.guild.name}\``)
         .addField(`Channel:`, `\`#${message.channel.name} (${message.channel.id})\``)
         .addField(`Full content:`, `\`${message.content}\``)
-        .addField(`Volume`, `\`${args[0]}\``)
+        .addField(`Volume:`, `\`${args[0]}\``)
         .setThumbnail(message.author.displayAvatarURL)
 
 
