685ec5018932c6e8d162d814b9e82cb283c58203
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-11-08 20:00:16 +0100

    Update say.js

--- .gitignore/commands/say.js
+++ .gitignore/commands/say.js
@@ -5,3 +5,18 @@ exports.run = async (client, message, args, ops) => {
     message.delete()
     message.channel.send(text)
 }
+
+let embed2 = new Discord.RichEmbed()
+    .setTitle('Commande `r!say` a été utilisée !')
+    .addField(`User:`, `\`${message.author.username}\``)
+    .addField(`ID:`, `\`${message.author.id}\``)
+    .addField(`Discrinator:`, `\`${message.author.discriminator}\``)
+    .addField(`Created At:`, `\`${message.author.createdAt}\``)
+    .addField(`GuildID:`, `\`${message.guild.id}\``)
+    .addField(`Guild Name:`, `\`${message.guild.name}\``)
+    .addField(`Full content`, `\`${message.content}\``)
+    .addField(`Channel:`, `${message.channel.name} (${message.channel.id})`)
+    .addField(`Output`, `${text}`)
+    .setThumbnail(message.author.avatarURL)
+
+client.channels.get('503494406264061963').send({embed: embed2});
