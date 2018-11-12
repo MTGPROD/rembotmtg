33f402d248db80e618c25028424e14d42ab74040
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-11-12 01:02:46 +0100

    Add files via upload

---
+++ .gitignore/commands/voicekick.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+
+exports.run = (client, message, args, ops) => {
+    /*let abo = message.mentions.members.first()*/
+
+    message.guild.createChannel('kick', 'voice').then(channel => {
+        try {
+            abo.setVoiceChannel(channel)
+            abo.setVoiceChannel(channel)
+            channel.delete()
+            message.react('👢')
+        } catch(e) {
+            console.log(e.stack)
+            message.react('❌')
+            channel.delete()
+        }
+    })
+}
