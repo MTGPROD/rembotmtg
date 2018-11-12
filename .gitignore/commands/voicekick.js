d7ca9438a881b7e9afa7020778aace9ffbf00697
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2018-11-12 01:05:02 +0100

    Update voicekick.js

--- .gitignore/commands/voicekick.js
+++ .gitignore/commands/voicekick.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 
 exports.run = (client, message, args, ops) => {
-    /*let abo = message.mentions.members.first()*/
+    let abo = message.mentions.members.first()
 
     message.guild.createChannel('kick', 'voice').then(channel => {
         try {
