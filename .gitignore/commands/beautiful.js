88030e4cc7510e08c88f2db074959c67818a5c01
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-01-09 14:29:43 +0100

    Create beautiful.js

---
+++ .gitignore/commands/beautiful.js
@@ -0,0 +1,19 @@
+const ARCAPI = require("arcadia.js-dev");
+const arcapi = new ARCAPI("a91e3faa988edd95a3a36e83575d7eb349be42049408f1bf1be9986d626d9cfd"); //Replace xxx to your arcadia's token
+ 
+let endpoint = "beautiful"; //Name of an endpoint
+let parameter = "url"; //First parameter of the endpoint (URL/TEXT)
+let url = message.author.avatarURL; //URL of the image
+ 
+arcapi.getImage(endpoint, parameter, url).then((buffer) => {
+    
+        message.channel.send({
+  files: [{
+    attachment: buffer,
+    name: `beautiful-${message.author.username}.gif`
+  }]
+})
+  .then(console.log)
+  .catch(console.error);
+
+});
