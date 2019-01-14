ff061a77a8e985a84b9bf71fd2ef6ab5f19be7e3
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-01-14 09:00:12 +0100

    Add files via upload

---
+++ .gitignore/commands/cookie.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+const help = require('./JSON/help.json')
+
+exports.run = (client, message, args, ops) => {
+    let target = message.mentions.members.first()
+
+if(target.id === message.author.id) return message.channel.send('You can\'t give yourself a cookie. <a:wegif:490534072892784650>')
+
+if(!args[0]) return message.channel.send(`\`\`\`
+${help.cmds_help_cookie}
+\`\`\``)
+
+    const embed = new Discord.RichEmbed()
+        .setDescription(`Gave you a :cookie: to **${target}**`)
+    message.channel.send(embed)
+}
