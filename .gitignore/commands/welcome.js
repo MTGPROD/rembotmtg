96e6a3cd675c14bc48dcad02a066402eabfde628
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-01-15 09:07:16 +0100

    Create welcome.js

---
+++ .gitignore/commands/welcome.js
@@ -0,0 +1,20 @@
+const ARCAPI = require("arcadia.js-dev");
+const arcapi = new ARCAPI(process.env.ARCATOKEN); //Replace xxx to your arcadia's token
+exports.run = (client, message, args, ops) => {
+let endpoint = "welcome"; //Name of an endpoint
+let parameter = "url"; //First parameter of the endpoint (URL/TEXT)
+let url = message.author.avatarURL; //URL of the image
+ 
+arcapi.getImage(endpoint, parameter, url).then((buffer) => {
+    
+        message.channel.send({
+  files: [{
+    attachment: buffer,
+    name: `welcome-${message.author.username}.gif`
+  }]
+})
+  .then(console.log)
+  .catch(console.error);
+
+});
+}
