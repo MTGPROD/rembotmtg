57038ffd820ec245bbf98d9dd710fa3c84ce1e13
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-01-15 09:35:41 +0100

    Add files via upload

---
+++ .gitignore/commands/topic.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+
+
+exports.run = (client, message, args, ops) => {
+    var topik = args.join(" ")
+
+    /*if(!client.guild.me.hasPermissions("MANAGE_CHANNELS")) return message.channel.send("I don't have the `MANAGE_CHANNELS` permission")*/
+
+message.channel.edit({ topic: topik })
+
+}
