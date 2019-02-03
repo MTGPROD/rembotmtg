e6257b0e3875df59949c2b6f3bc7ea32e5ad235f
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-02-03 11:16:38 +0100

    Create note.js

---
+++ .gitignore/commands/note.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js')
+
+exports.run = (client, message, args) => {
+     const Watora = new Discord.RichEmbed()
+        .setTitle('Watora 8.13/10')
+        .setThumbnail('https://cdn.discordapp.com/avatars/234234723314958339/a_e30f1a6556b4cc516cec8dd4192a65e9.gif')
+        .addField()
+}
