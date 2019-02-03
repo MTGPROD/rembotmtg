25050e9903d34b5aee3a32283807cc716cc8886d
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-02-03 18:38:59 +0100

    Update note.js

--- .gitignore/commands/note.js
+++ .gitignore/commands/note.js
@@ -3,8 +3,8 @@ const Discord = require('discord.js')
 exports.run = (client, message, args) => {
      const Watora = new Discord.RichEmbed()
         .setTitle('Bot: Watora, Author: Zenrac#0001')
-        .setThumbnail('https://cdn.discordapp.com/avatars/234234723314958339/a_e30f1a6556b4cc516cec8dd4192a65e9.gif')
-        .addField(':star: 8.13/10')
+        .setThumbnail('https://cdn.discordapp.com/avatars/220644154177355777/bd6b28005c26d079486063a4976dfb44.webp?size=1024')
+        .addField(':star: 8.13/10', '"Good loli."')
         
      message.channel.send(Watora)
 }
