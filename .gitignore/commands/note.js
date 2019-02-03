ad2f76613ae7494b9deac895229acc20e8f067b7
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-02-03 22:57:06 +0100

    Update note.js

--- .gitignore/commands/note.js
+++ .gitignore/commands/note.js
@@ -2,10 +2,31 @@ const Discord = require('discord.js')
 
 exports.run = (client, message, args) => {
      const Watora = new Discord.RichEmbed()
+
         .setTitle('Bot: Watora#2790, Author: Zenrac#0001')
+
         .setThumbnail('https://cdn.discordapp.com/avatars/220644154177355777/bd6b28005c26d079486063a4976dfb44.webp?size=1024')
+
         .addField(':star: 8.13/10', '"Good loli."')
+
         .setColor('#9f39ad') 
-        
-     message.channel.send(Watora)
+
+    const Ayana = new Discord.RichEmbed() 
+        .setTitle('Bot: Ayana#8911 Author: HcgRandon#0001')
+        .setThumbnail('https://cdn.discordapp.com/avatars/185476724627210241/6bfdcdeae0765c7065d3a68b67411b20.png?size=1024') 
+        .addField(':star: 9.33/10', '"Complete bot well done but, Ayana PRO has been abused. "') 
+        .setColor('#ed3468') 
+     
+     if(args[0] === 'watora') {
+          message.delete()
+          message.channel.send(Watora)
+     } 
+     
+     if(args[0] === 'ayana') {
+          message.delete()
+          message.channel.send(Ayana)
+     } 
+     
+     
+
 }
