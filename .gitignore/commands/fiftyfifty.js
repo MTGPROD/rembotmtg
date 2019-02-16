6a493f6326776b06ec5e22b6c329d0d6d2dcd064
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-02-16 02:41:12 +0100

    Update fiftyfifty.js

--- .gitignore/commands/fiftyfifty.js
+++ .gitignore/commands/fiftyfifty.js
@@ -1,12 +1,19 @@
 const Discord = require('discord.js')
 
 exports.run = (client, message, args) => {
+     
+     var API = [
+          "[**Cute Dog | Decapitated Head**](https://www.reddit.com/r/FiftyFifty/comments/aqbt1r/5050_cute_dog_sfw_decapitated_head_nsfw/)" 
+     ] 
+     
+     let fetched = API[Math.floor(Math.random() * API.length)];
+     
      const mainEmbed = new Discord.RichEmbed() 
           .setAuthor(message.author.username, message.author.displayAvatarURL)
           .setTitle('**50/50 Challenge**')
-          .setURL('https://reddit.com/r/5050pics')
+          .setURL('https://reddit.com/r/FiftyFifty')
           .setColor('#FF5700')
-          .setDescription(`test`)
+          .setDescription(API)
           .addImage(Fetched)
           .setFooter('From Reddit')
      message.channel.send(mainEmbed) 
