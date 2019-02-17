b20830f74afcb5facf374347c78e7a62c0a93b3b
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-02-17 14:57:42 +0100

    Update topic.js

--- .gitignore/commands/topic.js
+++ .gitignore/commands/topic.js
@@ -7,5 +7,6 @@ exports.run = (client, message, args, ops) => {
     /*if(!client.guild.me.hasPermissions("MANAGE_CHANNELS")) return message.channel.send("I don't have the `MANAGE_CHANNELS` permission")*/
 
 message.channel.edit({ topic: topik })
+message.channel.send(`The topic of this channel is \`${topik}\``)
 
 }
