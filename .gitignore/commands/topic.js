522f499253d76bd8ef3135ecdb266e3a6d2ddfbc
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-02-17 15:16:21 +0100

    Update topic.js

--- .gitignore/commands/topic.js
+++ .gitignore/commands/topic.js
@@ -3,10 +3,15 @@ const Discord = require('discord.js');
 
 exports.run = (client, message, args, ops) => {
     var topik = args.join(" ")
-
+    const prepik = message.channel.topic
     /*if(!client.guild.me.hasPermissions("MANAGE_CHANNELS")) return message.channel.send("I don't have the `MANAGE_CHANNELS` permission")*/
 
 message.channel.edit({ topic: topik })
+    
+const pik = new Discord.RichEmbed()
+    .setTitle(`Topic of the channel: #${message.channel.name}`)
+    .addField(`Before`, `**${prepik}**`, true)
+    .addField(`After`, `**${topik}**`, true)
 message.channel.send(`The topic of this channel is **${topik}**`)
 
 }
