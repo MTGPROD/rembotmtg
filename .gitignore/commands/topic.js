0e33ccdbd6751fc0a20a9168e2b2ac097ba1ed22
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-02-17 17:00:16 +0100

    Update topic.js

--- .gitignore/commands/topic.js
+++ .gitignore/commands/topic.js
@@ -1,10 +1,12 @@
 const Discord = require('discord.js');
 
 
+
 exports.run = (client, message, args, ops) => {
     var topik = args.join(" ")
     const prepik = message.channel.topic
-    if(!message.guild.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send(":x: You don't have the `MANAGE_CHANNELS` permission")*/
+    if(!message.guild.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send(":x: You don't have the `MANAGE_CHANNELS` permission")
+    if(!message.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return message.channel.send(":x: I don't have the `MANAGE_CHANNELS` permission")
 
 message.channel.edit({ topic: topik })
     
