1baf3de13287c6598559f02656e85fe2ab353c5b
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-02-18 07:58:43 +0100

    Update volume.js

--- .gitignore/commands/volume.js
+++ .gitignore/commands/volume.js
@@ -5,12 +5,13 @@ exports.run = (client, message, args, ops) => {
   let fetched = ops.active.get(message.guild.id);
   if(!fetched) return message.channel.send("There currently ins't any music playing in this guild !");
   if(message.member.voiceChannel !== message.guild.me.voiceChannel) return message.chanel.send("Sorry, you currently aren't in the same channel as the bot.")
-  if(isNaN(args[0]) || args[0] > 1000 || args[0] < 0) return message.channel.send(skip)
-  fetched.dispatcher.setVolume(args[0]/1000);
   const skip = new Discord.RichEmbed()
        .setTitle('Volume')
        .setDescription(`:speaker: ${args[0]}%`)
        .setFooter('You can put the volume between 0-1000')
+  if(isNaN(args[0]) || args[0] > 1000 || args[0] < 0) return message.channel.send(skip)
+  fetched.dispatcher.setVolume(args[0]/1000);
+ 
  message.channel.send(skip)
   
    let embed2 = new Discord.RichEmbed()
