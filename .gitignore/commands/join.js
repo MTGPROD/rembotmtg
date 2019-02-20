dc389f64e1783ca2c47abde275c363ca6b249a97
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-02-20 15:53:20 +0100

    Add files via upload

---
+++ .gitignore/commands/join.js
@@ -0,0 +1,5 @@
+exports.run = (client, message, args, ops) => {
+    if(!message.member.voiceChannel) return message.channel.send('Please connect to a voice channel')
+    message.member.voiceChannel.join()
+    message.react('✅')
+}
