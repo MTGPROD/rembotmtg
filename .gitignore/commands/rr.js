3b6334abc77099c722c4261060c5cec0a93ad80c
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-02-26 10:03:47 +0100

    Update rr.js

--- .gitignore/commands/rr.js
+++ .gitignore/commands/rr.js
@@ -1,5 +1,25 @@
 const Discord = require('discord.js')
 
 exports.run = (client, args, message, ops) => {
+    const COLORS = [
+        '#FFEF00', 
+        '#FFEF00', 
+        '#FF9A00', 
+        '#FF5500', 
+        '#FF9A00', 
+        '#D40C00', 
+        '#7F4FC9' 
+    ] 
+    
+  
+
+ 
+
+
+
+
+
+
+    
     member.guild.roles.find("name", "Rainbow").edit({ color: '#FFFFFF'})
 } 
