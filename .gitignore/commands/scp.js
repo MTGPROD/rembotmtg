37ff4905e5af007935270c53cf5731808620e2e9
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-02-26 14:29:03 +0100

    Update scp.js
    I finished the embed

--- .gitignore/commands/scp.js
+++ .gitignore/commands/scp.js
@@ -2,8 +2,10 @@ const Discord = require('discord.js')
 
 exports.run = (client, message, args) => {
 if(!args[0]) return message.channel.send('Usage: r!scp <001-4999>')
-if(args[0] === '001') {
+if(args[0] === '001-FR') {
 const e001 = new Discord.RichEmbed()
-.setDescription('En attente de classification [Bloqué]')
+.setTitle('SCP-001-FR')
+.setDescription(`En attente de classification [Bloqué]\n\n**NOTE GÉNÉRALE 001-Alpha:**\nAfin d'éviter les fuites sur la connaissance de SCP-001, plusieurs faux fichiers SCP-001 ont été créés avec le(s) vrai(s) fichier/fichiers. Tous les dossiers concernant la nature de SCP-001, y compris le(s) leurre/leurres, sont protégés par un agent mémétique destiné à provoquer immédiatement un arrêt cardiaque de tous les membres du personnel non-autorisés tentant d'accéder au(x) fichiers. Révéler la/les vraie(s) nature/natures de SCP-001 pour le grand public est une cause d\'exécution, sauf en vertu de ||████||-||███||-||██████||.`)
+message.channel.send(e001) 
 } 
 }
