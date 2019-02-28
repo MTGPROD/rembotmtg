cda1208888c7771f60f50c8acd5a051b6b3301f7
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-02-28 10:25:55 +0100

    Create channelinfo.js
    Creation of the commands, test version

---
+++ .gitignore/commands/channelinfo.js
@@ -0,0 +1,14 @@
+const Discord = require('discord. js')
+
+exports.run = (client, message, args) => {
+     if(args[0].length < 18) return message.channel.send(`\`${args[0]}\`is not an ID.`) 
+     if(args[0] === isNaN) return message.channel.send(`\`${args[0]}\`is not an ID.)
+     const chnl = client.channels.get(message.channel.id || args[0])
+     
+
+     const mainEmbed = new Discord.RichEmbed() 
+         .setTitle(`#chnl.name`)
+         .addField(`ID`, `${chnl.id}`, true)
+         .addField(`CreatedAt`, `${chnl.createdAt}`)
+     message.channel.send(mainEmbed) 
+} 
