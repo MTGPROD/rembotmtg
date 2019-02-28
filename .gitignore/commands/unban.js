1f867d0121c7f370f40a87b560f4eb5139d0ea9d
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-02-28 23:10:02 +0100

    Update unban.js

--- .gitignore/commands/unban.js
+++ .gitignore/commands/unban.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js"); 
 exports.run = async (client, message, args) => { 
 
-  if(args[0] < 18) return message.channel.send(`\`**${args[0]}**\` is not a valid id.`) 
+  if(args[0] < 20) return message.channel.send(`\`**${args[0]}**\` is not a valid id.`) 
   if(args[0] === isNaN) return message.channel.send(`\`**${args[0]}**\` is not a valid id.`) 
-  if(args[0] > 18) return message.channel.send(`\`**${args[0]}**\` is not a valid id.`)
+  if(args[0] > 20) return message.channel.send(`\`**${args[0]}**\` is not a valid id.`)
   if(!args[0]) return message.channel.send(`Usage: \`r!unban <userid>\``)
                                                       
   
