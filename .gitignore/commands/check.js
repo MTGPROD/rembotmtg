90c308e408be88f63fee32ec4e5485979f940088
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-03-01 00:49:45 +0100

    IU fix
    The emojis work

--- .gitignore/commands/check.js
+++ .gitignore/commands/check.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js') 
 
 exports.run = (client, message, args, ops) => {
-     var etat = ':good: Tous les systèmes sont opérationnel' 
+     var etat = '<:good:520015496133738497> Tous les systèmes sont opérationnel' 
      
-     if(client.ping > 250) return etat = ':normal: Redémarrage conseillé.' 
-     if(client.ping > 800) return etat = ':charged: Gros risques de lag, redémarrage requis.' 
-     if(client.ping > 2000) return etat = ":surcharged: Si ce n'est pas réglé dans quelques minutes, redémarrage urgent."  
+     if(client.ping > 250) return etat = '<:normal:520015530162389033> Redémarrage conseillé.' 
+     if(client.ping > 800) return etat = '<:charged:520015546293420062> Gros risques de lag, redémarrage requis.' 
+     if(client.ping > 2000) return etat = "<:surcharged:520015562210934785> Si ce n'est pas réglé dans quelques minutes, redémarrage urgent."  
    
      
      const mainEmbed = new Discord.RichEmbed()
