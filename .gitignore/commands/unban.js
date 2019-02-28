9f54bd534d57d075f9f85df49621edc23cc9f40f
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-02-28 22:22:45 +0100

    Code update
    This command is normally working.

--- .gitignore/commands/unban.js
+++ .gitignore/commands/unban.js
@@ -1,29 +1,23 @@
 const Discord = require("discord.js"); 
 exports.run = async (client, message, args) => { 
 
-  let logs = message.guild.channels.find("name", "logs");
-  if(!logs) return message.channel.send("Could not find a logs channel.");
+  if(args[0].length < 18) return message.channel.send(`\`${args[0]}\` is not a valid id.`) 
+  if(args[0] === isNaN) return message.channel.send(`\`${args[0]}\` is not a valid id.`) 
+  if(args[0].length > 18) return message.channel.send(`\`${args[0]\` is not a valid id.) 
+                                                      
+  
 
-  let user = message.mentions.users.first();
-  if(!user) return message.reply("Please mention a user");
+  let user = args[0] 
+  if(!user) return message.reply("Usage: `r!unban <id>");
 
   let reason = args.join(" ");
   if(!reason) reason = "No reason given";
 
   message.guild.member(user).unban(reason);
 
-  let logsEmbed = new Discord.RichEmbed() 
-  .setTitle("User UnBanned")
-  .setFooter("User UnBan Logs")
-  .setColor("#ff0000")
-  .setTimestamp()
-  .addField("UnBanned User:", `${user}, ID: ${user.id}`)
-  .addField("Reason:", reason)
-  .addField("Moderator:", `${message.author}, ID: ${message.author.id}`)
-  .addField("Time:", message.createdAt)
-  .addField("Channel:", message.channel)
+  
 
-  logs.send(logsEmbed);
+  message.channel.send(`You give a second chance to \`**${args[0]}**\` he shouldn't waste it.`) 
   
    let embed2 = new Discord.RichEmbed()
         .setTitle('Commande `r!unban` a été utilisée !')
