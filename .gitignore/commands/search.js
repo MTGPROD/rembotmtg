8ef0e7a56fc0a18db6951a39334b9f0ba588a7b9
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-03-01 12:55:18 +0100

    Variable fix
    M is not defined

--- .gitignore/commands/search.js
+++ .gitignore/commands/search.js
@@ -5,6 +5,7 @@ exports.run = (client, message, args, ops) => {
   search(args.join(' '), function(err, res) {
     if(err) return message.channel.send('Sorry, something wen wrong.');
     let videos = res.videos.slice(0, 5);
+    const filter = m => !isNaN(m.content) && m.content < videos.length+1 && m.content > 0;
     let resp = '';
     for(var i in videos) {
       resp += `**[${parseInt(i)+1}]:** - [${videos[i].title}](${this.videos[parseInt(m.content)-1]})\n`;
@@ -17,7 +18,7 @@ exports.run = (client, message, args, ops) => {
         .setTimestamp()
 
     message.channel.send(embed);
-    const filter = m => !isNaN(m.content) && m.content < videos.length+1 && m.content > 0;
+   
     const collector = message.channel.createMessageCollector(filter);
     collector.videos = videos;
     collector.once('collect', function(m) {
