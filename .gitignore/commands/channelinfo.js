17e0bad5d8f594005f4d880cd36d50ac6ad166ec
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-03-01 14:59:41 +0100

    Simply the command
    No need to enter an Id

--- .gitignore/commands/channelinfo.js
+++ .gitignore/commands/channelinfo.js
@@ -1,10 +1,8 @@
 const Discord = require('discord. js')
 
 exports.run = (client, message, args) => {
-     if(args[0].length < 18) return message.channel.send(`\`${args[0]}\`is not an ID.`) 
-     if(args[0] === isNaN) return message.channel.send(`\`${args[0]}\`is not an ID.)
-     const chnl = client.channels.get(message.channel.id || args[0])
      
+     var chnl = message.channel
 
      const mainEmbed = new Discord.RichEmbed() 
          .setTitle(`#${chnl.name}`)
