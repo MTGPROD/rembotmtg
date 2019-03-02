2cd8a6a7a6c9867cd98a8b1f3433a2ff6ca9c952
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-03-02 18:12:18 +0100

    Bug fix + Improvements
    Discord. js package

--- .gitignore/commands/channelinfo.js
+++ .gitignore/commands/channelinfo.js
@@ -1,12 +1,19 @@
-const Discord = require('discord. js')
+const Discord = require('discord.js')
 
 exports.run = (client, message, args) => {
      
      var chnl = message.channel
-
+     var notSafe = 'No (My nsfw commands are disabled on this channel.)'
+     if(chnl.nsfw === true) {
+          notSafe = 'Yes (My nsfw commands are enable.)' 
+     } 
+     
      const mainEmbed = new Discord.RichEmbed() 
          .setTitle(`#${chnl.name}`)
          .addField(`ID`, `${chnl.id}`, true)
+         .addField(`Mention`, `<#${chnl.id}>`, true) 
+         .addField(`Creation date`, chnl.createdAt) 
+         .addField(`Not Safe For Work ?`, `${notSafe}`)     
          .addField(`CreatedAt`, `${chnl.createdAt}`)
      message.channel.send(mainEmbed) 
 } 
