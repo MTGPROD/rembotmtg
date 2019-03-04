246f385bfd52e4bdd6c8ecae101ced7eeedb5b33
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-03-04 23:31:13 +0100

    Update beautiful.js

--- .gitignore/commands/beautiful.js
+++ .gitignore/commands/beautiful.js
@@ -3,13 +3,13 @@ const Discord = require('discord.js')
 
 const arcapi = new ARCAPI("095b8a5e1e02d6dafe3ca10f2b270138c391fa476f6c643b055445ad6d4a7828"); //Replace xxx to your arcadia's token
 
-exports.run = (message, client, args, ops) => {
+exports.run = (client, message, args) => {
 
 
 
 
 
-
+const target = message.mentions.users.first() || message.author 
 
 
 
@@ -23,7 +23,7 @@ let endpoint = "beautiful"; //Name of an endpoint
 
 let parameter = "url"; //First parameter of the endpoint (URL/TEXT)
 
-let url = message.author.avatarURL //URL of the image
+let url a targzr.vatarURL //URL of the image
 
 arcapi.getImage(endpoint, parameter, url).then(buffer => {
 
