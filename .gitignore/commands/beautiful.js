0bab8881e5350321df9e62d18df3fa4546c28d51
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-03-04 16:59:30 +0100

    Version 2 directly

---
+++ .gitignore/commands/beautiful.js
@@ -0,0 +1,17 @@
+const ARCAPI = require("arcadia.js-dev");
+
+const arcapi = new ARCAPI("095b8a5e1e02d6dafe3ca10f2b270138c391fa476f6c643b055445ad6d4a7828"); //Replace xxx to your arcadia's token
+
+var target = message.guild.member(message.mentions.members.first()) || message.author
+
+let endpoint = "beautiful"; //Name of an endpoint
+
+let parameter = "url"; //First parameter of the endpoint (URL/TEXT)
+
+let url = target.avatarURL //URL of the image
+
+arcapi.getImage(endpoint, parameter, url).then((buffer) => {
+
+    console.log(buffer); //Buffer
+
+});
