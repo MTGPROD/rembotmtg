7c5031a863a8384086f03372579a931ce28209b8
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-03-04 23:19:53 +0100

    Update beautiful.js

--- .gitignore/commands/beautiful.js
+++ .gitignore/commands/beautiful.js
@@ -5,17 +5,17 @@ const arcapi = new ARCAPI("095b8a5e1e02d6dafe3ca10f2b270138c391fa476f6c643b05544
 
 exports.run = (message, client, args, ops) => {
 
-var target = 0; // initialisation pour la visibilité de la variable
-if (message.mentions.users.first() != null) {
-   target =  message.mentions.users.first();
-} else {
-    target = message.author;
-}
 
 
 
 
- 
+
+
+
+
+
+
+ console.log(message.mentions.users.first()
 
 
 
@@ -23,7 +23,7 @@ let endpoint = "beautiful"; //Name of an endpoint
 
 let parameter = "url"; //First parameter of the endpoint (URL/TEXT)
 
-let url = target.avatarURL //URL of the image
+let url = message.author.avatarURL //URL of the image
 
 arcapi.getImage(endpoint, parameter, url).then(buffer => {
 
