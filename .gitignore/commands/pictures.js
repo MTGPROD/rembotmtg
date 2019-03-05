dd1b2c9026aa3a04950de926e1f8f82953f9f352
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-03-05 21:23:15 +0100

    Update pictures.js

--- .gitignore/commands/pictures.js
+++ .gitignore/commands/pictures.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
      const mainEmbed = new Discord.RichEmbed()
          .setTitle('Images generation help menu.')
          .setDescription('Thanks to the Arcadia API')
-         .addField(`Filter`, '`bluely`, `blur`, blurblack``, `blureen`, `blurey`, `blurple`, `convinvert`, `convmatrix`, `convolute`, `cyanly`, `displace`, `distortion`, `ghost`, `glitch`, `grayscale`, `implode`, `invert`, `magik`, `mosaic`, `offset`, `orangblur`, `orangly`, `pixelate`, `posterize`, `purply`, `sepia`, `time`, `yelloblur`') 
+         .addField(`Filter`, '`bluely`, `blur`, `blurblack`, `blureen`, `blurey`, `blurple`, `convinvert`, `convmatrix`, `convolute`, `cyanly`, `displace`, `distortion`, `ghost`, `glitch`, `grayscale`, `implode`, `invert`, `magik`, `mosaic`, `offset`, `orangblur`, `orangly`, `pixelate`, `posterize`, `purply`, `sepia`, `time`, `yelloblur`') 
                    
          .addField(`Generation`, '`beautiful`, `rainbow`, `discordlogo`, `gay`, `triggered`, `triggeredinvert`, `animeprotest`, `waifu`') 
          .addField('Text', 'Nope')
