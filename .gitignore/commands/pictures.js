f531292967ec2fe075da6cccf19751606b56da6e
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-03-05 21:20:19 +0100

    Update pictures.js

--- .gitignore/commands/pictures.js
+++ .gitignore/commands/pictures.js
@@ -4,8 +4,9 @@ exports.run = (client, message, args) => {
      const mainEmbed = new Discord.RichEmbed()
          .setTitle('Images generation help menu.')
          .setDescription('Thanks to the Arcadia API')
-         .addField(`Filter`, `Nope`)
-         .addField(`Generation`, '`beautiful`, `rainbow`, `discordlogo`, `gay`, `triggered`, `triggeredinvert`') 
+         .addField(`Filter`, '`bluely`, `blur`, blurblack``, `blureen`, `blurey`, `blurple`, `convinvert`, `convmatrix`, `convolute`, `cyanly`, `displace`, `distortion`, `ghost`, `glitch`, `grayscale`, `implode`, `invert`, `magik`, `mosaic`, `offset`, `orangblur`, `orangly`, `pixelate`, `posterize`, `purply`, `sepia`, `time`, `yelloblur`') 
+                   
+         .addField(`Generation`, '`beautiful`, `rainbow`, `discordlogo`, `gay`, `triggered`, `triggeredinvert`, `animeprotest`, `waifu`') 
          .addField('Text', 'Nope')
          .setFooter('Powered by Arcadia API')
      message.channel.send(mainEmbed)
