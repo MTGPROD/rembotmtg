b66e4d80370a39467ce443edfd0b9133b51b4928
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-03-10 21:43:46 +0100

    Update loveship.js

--- .gitignore/commands/loveship.js
+++ .gitignore/commands/loveship.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 exports.run = (client, message, args) => {
 var target = message.mentions.users.first() 
 
-let fetched = responses[Math.floor(Math.random() * 100)];
+let fetched = Math.floor(Math.random() * 100)
 
 const mainEmbed = new Discord.RichEmbed() 
      .setTitle(`Loveship ❤️❤️❤️`)
