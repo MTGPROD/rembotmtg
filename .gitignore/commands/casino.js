275be88b7f5a3638dd4ba62ca0ff33bf602cb578
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-03-11 19:48:25 +0100

    Create casino.js

---
+++ .gitignore/commands/casino.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+
+exports.run = (client, message, args) => {
+     let Machine = [':gem:', ':dvd:', ':pound:', ':cd:', ':moneybag:', ':credit_card:']
+
+     let slot1 = Machine[Math.floor(Math.random() * Machine.length)];
+     let slot2 = Machine[Math.floor(Math.random() * Machine.length)];
+     let slot3 = Machine[Math.floor(Math.random() * Machine.length)];
+var result = 'You lose!' 
+if(slot1 === slot2 === slot3) {
+     result = 'You have win!' 
+} 
+
+    const mainEmbed = new Discord.RichEmbed() 
+        .setTitle('Casino')
+        .setDescription(`${slot1} | ${slot2} | ${slot3}`)
+        .setFooter(result)
+    message.channel.send(mainEmbed)
+       
+    
+    
+} 
