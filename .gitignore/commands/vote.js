a2edd0d34565bc5b5b86a162008cd94573c4f5a5
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-03-24 14:50:08 +0100

    Update vote.js

--- .gitignore/commands/vote.js
+++ .gitignore/commands/vote.js
@@ -3,6 +3,6 @@ const Discord = require('discord.js')
 exports.run = (client, message, args) => {
      const mainEmbed = new Discord.RichEmbed() 
          .setTitle('Vote for me on DBL')
-         .setDescription('[Vote](https://discordbots.org/bot/495584319604260867/vote)')
+         .setDescription('Click here => [<:discordbotlist:338808864352763904>](https://discordbots.org/bot/488734399509168148/vote)')
      message.channel.send(mainEmbed) 
 } 
