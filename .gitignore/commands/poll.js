028e957c22d2c0912213bf5ad60bc171ce95f64e
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-06-04 18:08:15 +0100

    Fix
    Need the role Poll and don't need Admin permissions. 3.1v

--- .gitignore/commands/poll.js
+++ .gitignore/commands/poll.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 
 exports.run = async (client, message, args, ops) => {
     if(!message.member.roles.find(r => r.name === 'Poll')) return message.channel.send(`This requires the role: \`Poll\``);
-    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('This requires the permission: ADMIN');
+   // if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('This requires the permission: ADMIN');
     if(!args[0]) return message.channel.send('Proper Usage: `r!poll` question');
     const embed = new Discord.RichEmbed()
         .setTitle(`Poll Created By ${message.author.username}`)
         .setDescription(args.join(' '))
-        .setColor('#3395F7')
+        .setColor(message.member.highestRole.hexColor)
         .setFooter('React to vote.')
 
     let msg = await message.channel.send({embed: embed});
