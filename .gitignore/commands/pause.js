d8c902a07bb7d2b80f6ef0e3afe5ce3304ee7e6f
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-08-02 15:08:55 +0000

    Update pause.js

--- .gitignore/commands/pause.js
+++ .gitignore/commands/pause.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args, ops) => {
+  return message.channel.send("The music module is broken, please wait for the next update.") 
   let fetched = ops.active.get(message.guild.id);
   if(!fetched) return message.channel.send('There currently isn\'t any music playing in this guild !');
   if(message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send("Sorry, you aren't in the same channel as the music bot.");
