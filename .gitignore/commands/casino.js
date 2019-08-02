209111af45abb7ebd54cbf9279ac254e9ed93b8b
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-08-02 16:26:58 +0000

    Update casino.js

--- .gitignore/commands/casino.js
+++ .gitignore/commands/casino.js
@@ -54,6 +54,6 @@ if(args[0] === 'x5') {
     message.channel.send(mainEmbed)
        
  } 
-       let embed2 = new Discord.RichEmbed()     .setTitle('Commande `r!animes` a été utilisée !')     .addField(`User:`, `\`${message.author.username}\``)     .addField(`ID:`, `\`${message.author.id}\``)     .setColor('#36393F')     .addField(`Discrinator:`, `\`${message.author.discriminator}\``)     .addField(`Created At:`, `\`${message.author.createdAt}\``)     .addField(`GuildID:`, `\`${message.guild.id}\``)     .addField(`Guild Name:`, `\`${message.guild.name}\``)     .addField(`Channel:`, `\`#${message.channel.name} (${message.channel.id})\``)     .addField(`Full content:`, `\`${message.content}\``)     .addField(`Anime research:`, `\`${args}\``)     .setThumbnail(message.author.avatarURL)  
+       let embed2 = new Discord.RichEmbed()     .setTitle('Commande `r!casino` a été utilisée !')     .addField(`User:`, `\`${message.author.username}\``)     .addField(`ID:`, `\`${message.author.id}\``)     .setColor('#36393F')     .addField(`Discrinator:`, `\`${message.author.discriminator}\``)     .addField(`Created At:`, `\`${message.author.createdAt}\``)     .addField(`GuildID:`, `\`${message.guild.id}\``)     .addField(`Guild Name:`, `\`${message.guild.name}\``)     .addField(`Channel:`, `\`#${message.channel.name} (${message.channel.id})\``)     .addField(`Full content:`, `\`${message.content}\``)     .addField(`Anime research:`, `\`${args}\``)     .setThumbnail(message.author.avatarURL)  
 client.channels.get('503494406264061963').send({embed: embed2});
 } 
