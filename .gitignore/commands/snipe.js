3593781b3a9267d6c4e12ef47a1906e77761baaa
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-08-02 16:42:42 +0000

    Update snipe.js

--- .gitignore/commands/snipe.js
+++ .gitignore/commands/snipe.js
@@ -22,4 +22,7 @@ exports.run = (client, message, args) => {
         .setColor('#36393F')
 
     message.channel.send(embed);
+    
+       let embed2 = new Discord.RichEmbed()     .setTitle('Commande `r!snipe` a été utilisée !')     .addField(`User:`, `\`${message.author.username}\``)     .addField(`ID:`, `\`${message.author.id}\``)     .setColor('#36393F')     .addField(`Discrinator:`, `\`${message.author.discriminator}\``)     .addField(`Created At:`, `\`${message.author.createdAt}\``)     .addField(`GuildID:`, `\`${message.guild.id}\``)     .addField(`Guild Name:`, `\`${message.guild.name}\``)     .addField(`Channel:`, `\`#${message.channel.name} (${message.channel.id})\``)     .addField(`Full content:`, `\`${message.content}\``)     .addField(`Anime research:`, `\`${args}\``)     .setThumbnail(message.author.avatarURL)  
+client.channels.get('503494406264061963').send({embed: embed2});
 }
